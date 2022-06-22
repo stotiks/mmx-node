4893889d621570bb464567091a4da3fc3aa1a355
stotiks <47013208+stotiks@users.noreply.github.com>
2022-06-23 00:34:46 +0300

    dll path fix

--- cmake/win_pack.cmake
+++ cmake/win_pack.cmake
@@ -41,10 +41,8 @@ install(TARGETS vnxpasswd generate_passwd RUNTIME DESTINATION ./ COMPONENT appli
 install(TARGETS automy_basic_opencl RUNTIME DESTINATION ./ COMPONENT applications)
 install(TARGETS tx_bench RUNTIME DESTINATION ./ COMPONENT tools)
 
-find_package(ZLIB REQUIRED)
-find_package(OpenCL REQUIRED)
-install(FILES ${ZLIB_INCLUDE_DIRS}/../bin/zlib1.dll DESTINATION ./ COMPONENT applications)
-install(FILES ${OpenCL_INCLUDE_DIRS}/../bin/OpenCL.dll DESTINATION ./ COMPONENT applications)
+install(FILES $<TARGET_FILE_DIR:mmx>/zlib1.dll DESTINATION ./ COMPONENT applications)
+install(FILES $<TARGET_FILE_DIR:mmx>/OpenCL.dll DESTINATION ./ COMPONENT applications)
 
 install(DIRECTORY kernel/ DESTINATION kernel COMPONENT applications)
 install(DIRECTORY config/ DESTINATION config COMPONENT applications)
