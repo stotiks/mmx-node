9747b57d1eed055b28c32d45a5f374bb498ccae3
stotiks <47013208+stotiks@users.noreply.github.com>
2022-10-16 12:50:12 +0300

    win scripts fix

--- scripts/win/activate.cmd
+++ scripts/win/activate.cmd
@@ -1,45 +1,45 @@
 @ECHO OFF
 
-SET MMX_ENV=%~dp0
-SET MMX_ENV=%MMX_ENV:~0,-1%
-SET PATH=%PATH%;"%MMX_ENV%"
+SET "MMX_ENV=%~dp0"
+SET "MMX_ENV=%MMX_ENV:~0,-1%"
+SET "PATH=%PATH%; %MMX_ENV%"
 
 IF "%MMX_HOME%"=="" (
-	SET MMX_HOME=%USERPROFILE%\.mmx\
+	SET "MMX_HOME=%USERPROFILE%\.mmx\"
 )
 
-SET NETWORK=testnet7
-SET MMX_NETWORK=%NETWORK%\
+SET "NETWORK=testnet7"
+SET "MMX_NETWORK=%NETWORK%\"
 
 ECHO NETWORK=%NETWORK%
 ECHO MMX_HOME=%MMX_HOME%
 
-IF NOT EXIST "%MMX_HOME%\config\local\" (
-	XCopy "config\local_init" "%MMX_HOME%\config\local\" /S /F /Y
+IF NOT EXIST "%MMX_HOME%config\local" (
+	XCopy "config\local_init" "%MMX_HOME%config\local\" /S /F /Y
 	ECHO "Initialized config\local\ with defaults."
 )
 
-IF NOT EXIST "%MMX_HOME%\PASSWD" (
-	generate_passwd > %MMX_HOME%\PASSWD
+IF NOT EXIST "%MMX_HOME%PASSWD" (
+	generate_passwd > "%MMX_HOME%PASSWD"
 
-	SET /P PASSWD=<%MMX_HOME%\PASSWD
+	SET /P PASSWD=<"%MMX_HOME%PASSWD"
 	CALL ECHO PASSWD=%%PASSWD%%
 
-	CALL vnxpasswd -c config\default\ %MMX_HOME%\config\local\ -u mmx-admin -p %%PASSWD%%
+	CALL vnxpasswd -c config\default\ "%MMX_HOME%config\local\\" -u mmx-admin -p %%PASSWD%%
 )
 
-SET NEW_DB_VERSION=MMX2
-SET DB_VERSION_PATH=%MMX_HOME%\DB_VERSION
+SET "NEW_DB_VERSION=MMX2"
+SET "DB_VERSION_PATH=%MMX_HOME%DB_VERSION"
 
-IF NOT EXIST %DB_VERSION_PATH% GOTO DeleteDB
-SET /P DB_VERSION=<%DB_VERSION_PATH%
+IF NOT EXIST "%DB_VERSION_PATH%" GOTO DeleteDB
+SET /P DB_VERSION=<"%DB_VERSION_PATH%"
 IF %DB_VERSION% neq %NEW_DB_VERSION% GOTO DeleteDB
 GOTO afterDeleteDB
 
 :DeleteDB
 	RMDIR /s /q "%MMX_HOME%\%NETWORK%\db"
 	ECHO Deleted old DB
-	ECHO %NEW_DB_VERSION% > %DB_VERSION_PATH%
+	ECHO %NEW_DB_VERSION% > "%DB_VERSION_PATH%"
 :afterDeleteDB
 
 
