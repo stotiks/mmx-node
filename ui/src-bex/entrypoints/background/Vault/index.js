dad41b2fe7ae6db431b7c8136b803d0b6a155bae
stotiks <47013208+stotiks@users.noreply.github.com>
2025-04-09 17:47:46 +0300

    refactor: add removeData method to Vault class for improved data management

--- ui/src-bex/entrypoints/background/Vault/index.js
+++ ui/src-bex/entrypoints/background/Vault/index.js
@@ -1,4 +1,6 @@
+import { ECDSA_Wallet } from "@/mmx/wallet/ECDSA_Wallet";
 import VaultStorage from "./VaultStorage";
+import { seedToWords } from "@/mmx/wallet/mnemonic";
 
 let instance = null;
 
@@ -6,12 +8,33 @@ class Vault {
     #vaultStorage = new VaultStorage();
 
     unlock = async (password) => await this.#vaultStorage.unlock(password);
+    removeData = async () => await this.#vaultStorage.removeData();
 
     constructor() {
         if (!instance) {
             instance = this;
         }
         return instance;
+    }
+
+    async addWallet(seed) {
+        const wallet = new ECDSA_Wallet(seed);
+        const address = await wallet.getAddressAsync(0);
+
+        const data = await this.#vaultStorage.getData();
+        if (data.wallets && data.wallets.some((wallet) => wallet.address === address)) {
+            console.log("Wallet already exists");
+            throw new Error("Wallet already exists");
+        }
+
+        data.wallets ??= [];
+        data.wallets.push({
+            address,
+            seed,
+            mnemonic: seedToWords(seed), //debug
+        });
+
+        await this.#vaultStorage.setData(data);
     }
 
     async getWalletsAddresses() {
