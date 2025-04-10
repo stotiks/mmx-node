0a717bbcac6f94625114a25a1a6a87690c02733c
stotiks <47013208+stotiks@users.noreply.github.com>
2025-04-10 16:04:41 +0300

    refactor: update VaultStorage to streamline wallet management and improve data handling

--- ui/src-bex/entrypoints/background/Vault/EncryptedStorageItem.js
+++ ui/src-bex/entrypoints/background/Vault/EncryptedStorageItem.js
@@ -1,28 +1,38 @@
 import { storage } from "@wxt-dev/storage";
 import { decrypt, encrypt } from "@metamask/browser-passworder";
 
-export class EncryptedStorageItem {
-    #storageItemName;
+export class StorageItem {
+    #itemName;
 
     constructor(storageItemName) {
-        this.#storageItemName = storageItemName;
+        this.#itemName = storageItemName;
     }
 
     async exists() {
-        return (await storage.getItem(this.#storageItemName)) !== null;
+        return (await storage.getItem(this.#itemName)) !== null;
     }
 
+    async get() {
+        return await storage.getItem(this.#itemName);
+    }
+
+    async set(data) {
+        return await storage.setItem(this.#itemName, data);
+    }
+
+    async remove() {
+        return await storage.removeItem(this.#itemName);
+    }
+}
+
+export class EncryptedStorageItem extends StorageItem {
     async get(password) {
-        const encrypted = await storage.getItem(this.#storageItemName);
+        const encrypted = super.get();
         return await decrypt(password, encrypted);
     }
 
     async set(data, password) {
         const encrypted = await encrypt(password, data);
-        return await storage.setItem(this.#storageItemName, encrypted);
-    }
-
-    async remove() {
-        return await storage.removeItem(this.#storageItemName);
+        return await super.set(encrypted);
     }
 }
