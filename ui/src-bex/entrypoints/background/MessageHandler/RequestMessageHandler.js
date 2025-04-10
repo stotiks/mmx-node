613129a119786aed9b4020e23b1b620538eac06c
stotiks <47013208+stotiks@users.noreply.github.com>
2025-04-10 18:48:06 +0300

    refactor: move openPopup function to utils and update RequestMessageHandler

--- ui/src-bex/entrypoints/background/MessageHandler/RequestMessageHandler.js
+++ ui/src-bex/entrypoints/background/MessageHandler/RequestMessageHandler.js
@@ -1,6 +1,7 @@
 import { MessageHandlerBase } from "./MessageHandlerBase";
 import { getNodeInfo } from "../queries";
 import vault from "../Vault";
+import { openPopup } from "../utils";
 
 export class RequestMessageHandler extends MessageHandlerBase {
     static mmx_blockNumber = async () => {
@@ -10,5 +11,10 @@ export class RequestMessageHandler extends MessageHandlerBase {
 
     static mmx_requestAccounts = async () => {
         return await vault.getWalletsAddresses();
+    };
+
+    static dev_test_openPopup = async () => {
+        openPopup();
+        return "dev_test_openPopup";
     };
 }
