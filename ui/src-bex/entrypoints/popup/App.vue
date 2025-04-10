ec697c6877ceb7a35dd40871f18e81a428c8eea8
stotiks <47013208+stotiks@users.noreply.github.com>
2025-04-10 18:37:51 +0300

    setup quasar for popup

--- ui/src-bex/entrypoints/popup/App.vue
+++ ui/src-bex/entrypoints/popup/App.vue
@@ -1,7 +1,19 @@
 <template>
-    <div>hello</div>
+    <q-layout view="hHh lpR fFf">
+        <q-header elevated>
+            <q-bar>
+                <q-btn dense flat :icon="mdiAlphaFBox" />
+                <div class="text-weight-bold">Furry Vault</div>
+            </q-bar>
+        </q-header>
+        <q-page-container>
+            <q-page padding>
+                <m-chip>qwe</m-chip>
+            </q-page>
+        </q-page-container>
+    </q-layout>
 </template>
 
-<script setup></script>
-
-<style lang="scss" scoped></style>
+<script setup>
+import { mdiAlphaFBox } from "@mdi/js";
+</script>
