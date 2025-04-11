b7be5458595680bbd1439b3dfa9f515c5e82d69c
stotiks <47013208+stotiks@users.noreply.github.com>
2025-04-11 05:12:58 +0300

    refactor: streamline openNotification logic by removing redundant promise and console logs

--- ui/src-bex/utils/openNotification.js
+++ ui/src-bex/utils/openNotification.js
@@ -15,52 +15,44 @@ export const openNotification = async () => {
     }
 
     if (notificationWindowId) {
-        console.log("Notification is already open.");
-        return new Promise((resolve) => {
-            const checkPopupLoaded = setInterval(() => {
-                if (isNotificationLoaded) {
-                    clearInterval(checkPopupLoaded);
-                    resolve();
-                }
-            }, 100);
+        //console.log("Notification is already open.");
+    } else {
+        const currentWindow = await chrome.windows.getCurrent();
+
+        const rightOffset = 80;
+        const topOffset = 80;
+        const width = 400;
+        const height = 600;
+
+        const newWindow = await chrome.windows.create({
+            url: chrome.runtime.getURL("notification.html"),
+            type: "popup",
+            width,
+            height,
+            focused: true,
+            top: currentWindow.top + topOffset,
+            left: currentWindow.left + currentWindow.width - width - rightOffset,
+        });
+
+        notificationWindowId = newWindow.id;
+        const tabId = newWindow.tabs[0].id;
+
+        chrome.tabs.onUpdated.addListener(function listener(tabIdUpdated, changeInfo) {
+            if (tabId === tabIdUpdated && changeInfo.status === "complete") {
+                chrome.tabs.onUpdated.removeListener(listener);
+                isNotificationLoaded = true;
+                //console.log("Window content fully loaded");
+            }
+        });
+
+        chrome.tabs.onRemoved.addListener(function listener(tabIdUpdated) {
+            if (tabId === tabIdUpdated) {
+                chrome.tabs.onRemoved.removeListener(listener);
+                isNotificationLoaded = false;
+                //console.log("Window closed");
+            }
         });
     }
-
-    const currentWindow = await chrome.windows.getCurrent();
-
-    const rightOffset = 80;
-    const topOffset = 80;
-    const width = 400;
-    const height = 600;
-
-    const newWindow = await chrome.windows.create({
-        url: chrome.runtime.getURL("notification.html"),
-        type: "popup",
-        width,
-        height,
-        focused: true,
-        top: currentWindow.top + topOffset,
-        left: currentWindow.left + currentWindow.width - width - rightOffset,
-    });
-
-    notificationWindowId = newWindow.id;
-    const tabId = newWindow.tabs[0].id;
-
-    chrome.tabs.onUpdated.addListener(function listener(tabIdUpdated, changeInfo) {
-        if (tabId === tabIdUpdated && changeInfo.status === "complete") {
-            chrome.tabs.onUpdated.removeListener(listener);
-            isNotificationLoaded = true;
-            //console.log("Window content fully loaded");
-        }
-    });
-
-    chrome.tabs.onRemoved.addListener(function listener(tabIdUpdated) {
-        if (tabId === tabIdUpdated) {
-            chrome.tabs.onRemoved.removeListener(listener);
-            isNotificationLoaded = false;
-            //console.log("Window closed");
-        }
-    });
 
     return new Promise((resolve) => {
         const checkPopupLoaded = setInterval(() => {
