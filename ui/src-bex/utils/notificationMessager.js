09616e6ab3a213d3c77e9a44312100614b3d16e6
stotiks <47013208+stotiks@users.noreply.github.com>
2025-04-11 04:45:30 +0300

    feat: enhance notification handling by adding loading state and improving openNotification logic

--- ui/src-bex/utils/notificationMessager.js
+++ ui/src-bex/utils/notificationMessager.js
@@ -1,14 +1,9 @@
-import { openNotification } from "@bex/utils/openNotification";
+import { openNotification, isNotificationLoaded } from "@bex/utils/openNotification";
 import { internalMessenger } from "@bex/utils/internalMessenger";
 
 export const notificationMessaging = {
     sendMessage: async (message) => {
-        try {
-            await internalMessenger.sendMessage("notification", { method: "ping" });
-        } catch (e) {
-            const popup = await openNotification();
-        }
-
+        await openNotification();
         await internalMessenger.sendMessage("notification", message);
     },
 };
