e0ee04f2d6813d1f2b892f86e050923707588a21
stotiks <47013208+stotiks@users.noreply.github.com>
2025-04-12 21:12:41 +0300

    lint

--- ui/src-bex/messaging/utils/callbackGuard.js
+++ ui/src-bex/messaging/utils/callbackGuard.js
@@ -1,7 +1,11 @@
+// eslint-disable-next-line no-restricted-imports
 import { isInternalEndpoint } from "webext-bridge";
 
+// https://serversideup.net/open-source/webext-bridge/docs/guide/security
 export const callbackGuard = (callback) => (message) => {
     const { sender, id } = message;
+
+    // Respond only if request is from 'devtools', 'content-script', 'popup', 'options', or 'background' endpoint
     if (isInternalEndpoint(sender)) {
         return callback(message);
     } else {
