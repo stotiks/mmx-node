55ce364bd4ed73cc6831ea53a18b28a14a76fc96
stotiks <47013208+stotiks@users.noreply.github.com>
2025-04-12 06:26:41 +0300

    app

--- ui/src-bex/entrypoints/popup/App.vue
+++ ui/src-bex/entrypoints/popup/App.vue
@@ -8,10 +8,18 @@
         </q-header> -->
         <q-page-container>
             <q-page padding>
-                <template v-for="account in accounts" :key="account">
-                    <div>
-                        <m-chip copy>{{ account }}</m-chip>
-                    </div>
+                <template v-if="isVaultLocked">
+                    Vault is locked
+
+                    <q-input v-model="password" type="password" label="Password" filled dense />
+                    <q-btn @click="doRequest({ method: 'unlockVault', params: password })">Unlock</q-btn>
+                </template>
+                <template v-else>
+                    <template v-for="account in accounts" :key="account">
+                        <div>
+                            <m-chip copy>{{ account }}</m-chip>
+                        </div>
+                    </template>
                 </template>
             </q-page>
         </q-page-container>
@@ -21,14 +29,18 @@
 <script setup>
 import { mdiAlphaFBox } from "@mdi/js";
 
+const password = ref("password");
+
+const $q = useQuasar();
 import { internalMessenger } from "@bex/messaging/popup";
-const accounts = computedAsync(
-    async () => await internalMessenger.sendMessage("request", { method: "mmx_requestAccounts" }),
-    null,
-    {
-        onError: (error) => {
-            throw error;
-        },
+const doRequest = async (payload) => {
+    try {
+        return await internalMessenger.sendMessage("notification", payload);
+    } catch (error) {
+        $q.notify({ type: "negative", message: error.message });
     }
-);
+};
+
+const isVaultLocked = computedAsync(() => doRequest({ method: "isVaultLocked" }), false);
+const accounts = computedAsync(async () => await doRequest({ method: "getWalletsAddresses" }), []);
 </script>
