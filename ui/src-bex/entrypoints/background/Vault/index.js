56493c241c0132298263f40d04e7ed400a81f034
stotiks <47013208+stotiks@users.noreply.github.com>
2025-04-12 06:26:11 +0300

    vault

--- ui/src-bex/entrypoints/background/Vault/index.js
+++ ui/src-bex/entrypoints/background/Vault/index.js
@@ -4,24 +4,91 @@ import { seedToWords } from "@/mmx/wallet/mnemonic";
 
 let instance = null;
 
-class Vault {
-    #vaultStorage = new VaultStorage();
+import { EncryptedStorageItem } from "./StorageItem";
 
-    unlock = async (password) => await this.#vaultStorage.unlock(password);
-    removeData = async () => await this.#vaultStorage.removeData();
+class Vault {
+    #password = null;
+    #walletStorage;
+    #wallets;
 
     constructor() {
         if (!instance) {
+            this.#walletStorage = new EncryptedStorageItem("local:wallets");
             instance = this;
         }
         return instance;
     }
 
-    async addWallet(seed, password) {
+    get isLocked() {
+        return this.#password === null;
+    }
+
+    async lock() {
+        await this.save();
+        await this.#unload();
+    }
+
+    async unlock(password) {
+        if (!this.isLocked) {
+            throw new Error("Vault is unlocked already");
+        }
+
+        await this.#load(password);
+        this.#password = password;
+    }
+
+    async #load(password) {
+        if (await this.#walletStorage.exists()) {
+            this.#wallets = await this.#walletStorage.get(password);
+        } else {
+            this.#wallets = [];
+        }
+    }
+
+    async #unload() {
+        this.#wallets = null;
+        this.#password = null;
+    }
+
+    async updatePassword(password) {
+        if (this.isLocked) {
+            throw new Error("Vault is locked");
+        }
+
+        this.#password = password;
+        this.save();
+    }
+
+    async removeData() {
+        if (!this.isLocked) {
+            throw new Error("Vault is unlocked, cannot remove data");
+        }
+        await this.#walletStorage.remove();
+    }
+
+    async save() {
+        if (this.isLocked) {
+            throw new Error("Vault is locked");
+        }
+        await this.#walletStorage.set(this.#wallets, this.#password);
+    }
+
+    #getWallets() {
+        if (this.isLocked) {
+            throw new Error("Vault is locked");
+        }
+        return this.#wallets;
+    }
+
+    async addWallet(seed, password = "") {
+        if (this.isLocked) {
+            throw new Error("Vault is locked");
+        }
+
         const wallet = new ECDSA_Wallet(seed, password);
         const address = await wallet.getAddressAsync(0);
 
-        const wallets = this.#vaultStorage.getWallets();
+        const wallets = this.#getWallets();
 
         if (wallets.some((wallet) => wallet.address === address)) {
             throw new Error("Wallet already exists");
@@ -34,11 +101,11 @@ class Vault {
             mnemonic: seedToWords(seed), //debug
         });
 
-        await this.#vaultStorage.save();
+        await this.save();
     }
 
     async getWalletsAddresses() {
-        const wallets = this.#vaultStorage.getWallets();
+        const wallets = this.#getWallets();
         return wallets.map((wallet) => wallet.address);
     }
 }
