b8ca9f0b4ec8493b7e4be44e089a11340dd891a4
stotiks <47013208+stotiks@users.noreply.github.com>
2025-04-13 01:24:39 +0300

    toCamelCase

--- ui/src-bex/entrypoints/background/MessageHandler/MessageHandlerBase.js
+++ ui/src-bex/entrypoints/background/MessageHandler/MessageHandlerBase.js
@@ -1,7 +1,12 @@
+const toCamelCase = (str) => {
+    return str.replace(/-([a-zA-Z])/g, (match, group1) => group1.toUpperCase());
+};
+
 export class MessageHandlerBase {
     static async handle(message) {
-        const { method, params } = message.data;
+        const { method: _method, params } = message.data;
 
+        const method = toCamelCase(_method);
         const handler = this[method];
         if (!handler) {
             throw new Error(`unknown method: ${method}`);
