eb9a121f50c550f79d6cfd2d2dd5ec8b66f6710e
stotiks <47013208+stotiks@users.noreply.github.com>
2025-04-12 15:11:51 +0300

    lint

--- ui/src-bex/messaging/window.js
+++ ui/src-bex/messaging/window.js
@@ -1,3 +1,4 @@
+// eslint-disable-next-line no-restricted-imports
 import { onMessage, sendMessage, setNamespace } from "webext-bridge/window";
 export const windowMessenger = { onMessage, sendMessage };
 
