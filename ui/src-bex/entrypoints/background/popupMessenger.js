c3ac16d3fc1ff243ebe1c2df731a54d4fe9d5aca
stotiks <47013208+stotiks@users.noreply.github.com>
2025-04-14 02:17:55 +0300

    reorg

--- ui/src-bex/entrypoints/background/popupMessenger.js
+++ ui/src-bex/entrypoints/background/popupMessenger.js
@@ -1,9 +1,9 @@
 import { openNotification } from "@bex/entrypoints/background/openNotification";
 import { internalMessenger } from "@bex/messaging/background";
 
-export const notificationMessenger = {
+export const popupMessenger = {
     sendMessage: async (data) => {
         await openNotification();
-        await internalMessenger.sendMessage("notification", data, "popup");
+        await internalMessenger.sendMessage("popup", data, "popup");
     },
 };
