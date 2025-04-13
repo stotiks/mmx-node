190c3e55636bd51e3d3b8eb9ef425a0547119dfb
stotiks <47013208+stotiks@users.noreply.github.com>
2025-04-14 02:29:34 +0300

    reorg

---
+++ ui/src-bex/entrypoints/popup/MessageHandlers/useVaultMessageHandler.js
@@ -0,0 +1,32 @@
+import { internalMessenger } from "@bex/messaging/popup";
+import { MessageHandlerBase } from "@bex/messaging/utils/MessageHandlerBase";
+
+export const useVaultMessageHandler = () => {
+    const $q = useQuasar();
+
+    class VaultMessageHandler extends MessageHandlerBase {
+        static unlocked = async () => {
+            $q.notify({ type: "positive", message: "Vault unlocked" });
+        };
+
+        static locked = async () => {
+            $q.notify({ type: "positive", message: "Vault locked" });
+        };
+
+        static passwordUpdated = async () => {
+            $q.notify({ type: "positive", message: "Password updated" });
+        };
+
+        static walletAdded = async () => {
+            $q.notify({ type: "positive", message: "Wallet added" });
+        };
+
+        static walletRemoved = async () => {
+            $q.notify({ type: "positive", message: "Wallet removed" });
+        };
+    }
+
+    internalMessenger.onMessage("vault", async (message) => {
+        return await VaultMessageHandler.handle(message);
+    });
+};
