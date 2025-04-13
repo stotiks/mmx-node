02f8339436840826f9a4520320c45239151a7545
stotiks <47013208+stotiks@users.noreply.github.com>
2025-04-14 00:23:39 +0300

    initializeExtension

--- ui/src-bex/entrypoints/background/index.js
+++ ui/src-bex/entrypoints/background/index.js
@@ -7,7 +7,6 @@ import vault from "./Vault";
 export default defineBackground(() => {
     console.log("Hello from background world!");
 
-    //openPopup();
     // browser.runtime.onInstalled.addListener(async ({ reason }) => {
     //     if (reason !== "install") return;
     //     // Open a tab on install
@@ -16,34 +15,34 @@ export default defineBackground(() => {
     //         active: true,
     //     });
     // });
-    // const seed = randomSeed();
-    // console.log(seedToWords(seed));
 
     const initializeExtension = async () => {
-        await vault.removeData();
-        await vault.unlock("password");
-        await vault.addWallet(randomSeed());
-        await vault.addWallet(randomSeed());
-        await vault.addWallet(randomSeed());
+        //await vault.initAsync();
+
+        // await vault.removeData();
+        // await vault.unlock("password");
+        // await vault.addWallet(randomSeed());
+        // await vault.addWallet(randomSeed());
+        // await vault.addWallet(randomSeed());
         // await vault.lock();
+
+        //process messages from injected provider
+        internalMessenger.onWindowMessage("request", async (message) => {
+            console.log("Received from inpage:", message);
+            return await RequestMessageHandler.handle(message);
+        });
+
+        //process messages from popup/notification
+        internalMessenger.onMessage("notification", async (message) => {
+            console.log("Received from notification/popup:", message);
+            return await NotificationMessageHandler.handle(message);
+        });
+
+        //forward events from vault to popup
+        vault.on("<any>", async (eventName, ...args) => {
+            await internalMessenger.sendMessage("vault", { method: eventName, params: args }, "popup");
+        });
     };
 
     initializeExtension();
-
-    //process messages from injected provider
-    internalMessenger.onWindowMessage("request", async (message) => {
-        console.log("Received from inpage:", message);
-        return await RequestMessageHandler.handle(message);
-    });
-
-    //process messages from popup/notification
-    internalMessenger.onMessage("notification", async (message) => {
-        console.log("Received from notification/popup:", message);
-        return await NotificationMessageHandler.handle(message);
-    });
-
-    //forward events from vault to popup
-    vault.on("<any>", async (eventName, ...args) => {
-        await internalMessenger.sendMessage("vault", { method: eventName, params: args }, "popup");
-    });
 });
