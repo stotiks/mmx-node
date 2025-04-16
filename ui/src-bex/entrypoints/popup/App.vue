47e286309864c38994cc9dfcd9ce5b2171613d77
stotiks <47013208+stotiks@users.noreply.github.com>
2025-04-17 01:30:29 +0300

    pinia init

--- ui/src-bex/entrypoints/popup/App.vue
+++ ui/src-bex/entrypoints/popup/App.vue
@@ -8,17 +8,17 @@
         </q-header> -->
         <q-page-container>
             <q-page padding>
-                <template v-if="isVaultLocked">
+                <template v-if="isLocked">
                     Vault is locked
                     <q-input v-model="password" type="password" label="Password" filled dense />
-                    <q-btn @click="handleUnlock">Unlock</q-btn>
+                    <q-btn @click="handleUnlockAsync">Unlock</q-btn>
                 </template>
                 <template v-else>
                     <div class="q-gutter-y-sm">
-                        <q-btn @click="handleLock">Lock</q-btn>
+                        <q-btn @click="handleLockAsync">Lock</q-btn>
                         <q-input v-model="password" type="password" label="Password" filled dense />
                         <q-input v-model="newPassword" type="password" label="Password" filled dense />
-                        <q-btn @click="handlePasswordUpdate">Update</q-btn>
+                        <q-btn @click="handleUpdatePasswordAsync">Update</q-btn>
                         <template v-for="account in accounts" :key="account">
                             <div>
                                 <m-chip copy>{{ account }}</m-chip>
@@ -38,66 +38,30 @@ const password = ref("password");
 const newPassword = ref("password");
 
 const $q = useQuasar();
-import { internalMessenger } from "@bex/messaging/popup";
-const sendMessage = async (payload) => {
+const tryCatchWrapper = async (fn) => {
     try {
-        return await internalMessenger.sendMessage("notification", payload);
+        return await fn();
     } catch (error) {
         $q.notify({ type: "negative", message: error.message });
     }
 };
 
-const handleUnlock = async () => {
-    await sendMessage({ method: "unlockVault", params: { password: password.value } });
+import { useVaultStore } from "@bex/entrypoints/popup/stores/vault.js";
+const vaultStore = useVaultStore();
+const { isLocked, accounts } = storeToRefs(vaultStore);
+
+const handleUnlockAsync = async () => {
+    await tryCatchWrapper(() => vaultStore.unlockAsync(password.value));
 };
 
-const handleLock = async () => {
-    await sendMessage({ method: "lockVault" });
+const handleLockAsync = async () => {
+    await tryCatchWrapper(() => vaultStore.lockAsync());
 };
 
-const handlePasswordUpdate = async () => {
-    await sendMessage({
-        method: "updatePassword",
-        params: { password: password.value, newPassword: newPassword.value },
-    });
+const handleUpdatePasswordAsync = async () => {
+    await tryCatchWrapper(() => vaultStore.updatePasswordAsync(password.value, newPassword.value));
 };
 
-const accounts = ref([]);
-const isVaultLocked = ref(true);
-onMounted(async () => {
-    isVaultLocked.value = await sendMessage({ method: "isVaultLocked" });
-});
-
-watchEffect(async () => {
-    if (isVaultLocked.value) {
-        accounts.value = [];
-    } else {
-        accounts.value = await sendMessage({ method: "getWalletsAddresses" });
-    }
-});
-
-import { MessageHandlerBase } from "@bex/messaging/utils/MessageHandlerBase";
-class VaultMessageHandler extends MessageHandlerBase {
-    static unlocked = async () => {
-        isVaultLocked.value = false;
-        $q.notify({ type: "positive", message: "Vault is unlocked" });
-    };
-
-    static locked = async () => {
-        isVaultLocked.value = true;
-        $q.notify({ type: "positive", message: "Vault is locked" });
-    };
-
-    static walletsLoaded = async () => {
-        accounts.value = await sendMessage({ method: "getWalletsAddresses" });
-    };
-
-    static passwordUpdated = async () => {
-        $q.notify({ type: "positive", message: "Password updated" });
-    };
-}
-
-internalMessenger.onMessage("vault", async (message) => {
-    return await VaultMessageHandler.handle(message);
-});
+import { useVaultMessageHandler } from "@bex/entrypoints/popup/composables/useVaultMessageHandler";
+useVaultMessageHandler();
 </script>
--- ui/src-bex/entrypoints/popup/plugins.js
+++ ui/src-bex/entrypoints/popup/plugins.js
@@ -1,15 +1,29 @@
 // Plugins
 import { Quasar, quasarConfig } from "@/plugins/quasar";
-//import pinia from "@/plugins/pinia";
+// import pinia from "@/plugins/pinia";
 ////import router from "@/plugins/router";
 //import i18n from "@/plugins/i18n";
 // import VueQueryPlugin, { vueQueryPluginOptions } from "@/plugins/query";
 //import highlight from "@/plugins/highlight";
 
+import { createPinia } from "pinia";
+
+// import router from "@/plugins/router";
+// import { useQueryClient } from "@tanstack/vue-query";
+
+const pinia = createPinia();
+
+pinia.use(({ store }) => {
+    // store.router = markRaw(router);
+    // store.queryClient = markRaw(useQueryClient());
+});
+
+export default pinia;
+
 export function registerPlugins(app) {
     app.use(Quasar, quasarConfig);
     //app.use(router);
-    //app.use(pinia);
+    app.use(pinia);
     //app.use(i18n);
     // app.use(VueQueryPlugin, vueQueryPluginOptions);
     //app.use(highlight);
