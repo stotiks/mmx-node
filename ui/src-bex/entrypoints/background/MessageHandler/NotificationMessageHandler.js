f7f35a7cef9fd18600fb2954078353db6bcc9644
stotiks <47013208+stotiks@users.noreply.github.com>
2025-04-17 01:30:29 +0300

    pinia init

--- ui/src-bex/entrypoints/background/MessageHandler/NotificationMessageHandler.js
+++ ui/src-bex/entrypoints/background/MessageHandler/NotificationMessageHandler.js
@@ -2,20 +2,20 @@ import vault from "../Vault";
 import { MessageHandlerBase } from "@bex/messaging/utils/MessageHandlerBase";
 
 export class NotificationMessageHandler extends MessageHandlerBase {
-    static isVaultLocked = async () => {
+    static isLocked = async () => {
         return vault.isLocked;
     };
 
     static unlockVault = async ({ password }) => {
-        await vault.unlockAsync(password);
+        return await vault.unlockAsync(password);
     };
 
     static lockVault = async () => {
-        await vault.lockAsync();
+        return await vault.lockAsync();
     };
 
     static updatePassword = async ({ password, newPassword }) => {
-        await vault.updatePasswordAsync(password, newPassword);
+        return await vault.updatePasswordAsync(password, newPassword);
     };
 
     static getWalletsAddresses = () => {
