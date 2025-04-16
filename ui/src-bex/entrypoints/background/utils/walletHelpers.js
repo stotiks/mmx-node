233adef45e2a286c3cdc175e8103b4721060f44e
stotiks <47013208+stotiks@users.noreply.github.com>
2025-04-16 22:55:28 +0300

    mmx_signMessage

--- ui/src-bex/entrypoints/background/utils/walletHelpers.js
+++ ui/src-bex/entrypoints/background/utils/walletHelpers.js
@@ -1,21 +1,29 @@
 import { bytesToHex, hexToBytes } from "@noble/hashes/utils";
 import vault from "../storage/vault";
+import { sha256 } from "@noble/hashes/sha256";
 
-export const getPubKeyAsync = async (address) => {
-    if (address == null) {
-        address = vault.getCurrentWalletAddress();
-    }
+const getWalletByAddress = async (address) => {
+    return vault.getWallets().find((wallet) => wallet.address === address);
+};
 
+export const getCurrentWallet = () => {
+    const address = vault.getCurrentWalletAddress();
+    return getWalletByAddress(address);
+};
+
+export const getPubKeyAsync = async (address = vault.getCurrentWalletAddress()) => {
     const ecdsaWallet = await vault.getECDSAWalletAsync(address);
     const { pubKey } = await ecdsaWallet.getKeysAsync(0);
 
     return bytesToHex(pubKey).toUpperCase();
 };
 
-export const getWalletByAddress = async (address) => {
-    return vault.getWallets().find((wallet) => wallet.address === address);
-};
+export const signMessageAsync = async (msg, address = vault.getCurrentWalletAddress()) => {
+    const msgWithPrefix = `MMX/sign_message/${msg}`;
+    const msgHash = sha256(msgWithPrefix);
 
-export const getCurrentWalletAsync = async () => {
-    return getWalletByAddress(vault.getCurrentWalletAddress());
+    const ecdsaWallet = await vault.getECDSAWalletAsync(address);
+
+    const signature = await ecdsaWallet.signMsgAsync(address, msgHash);
+    return signature;
 };
