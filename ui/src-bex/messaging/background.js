1491e5e1c2a318ca6e1bd1f822e5f27830edcc7f
stotiks <47013208+stotiks@users.noreply.github.com>
2025-04-17 03:48:33 +0300

    sendMessageWrapper

--- ui/src-bex/messaging/background.js
+++ ui/src-bex/messaging/background.js
@@ -1,13 +1,15 @@
 // eslint-disable-next-line no-restricted-imports
-import { onMessage as _onMessage, sendMessage } from "webext-bridge/background";
+import { onMessage as _onMessage, sendMessage as _sendMessage } from "webext-bridge/background";
 
 import { callbackGuard } from "./utils/callbackGuard";
-const onMessage = (messageId, callback) => _onMessage(messageId, callbackGuard(callback));
+import { sendMessageWrapper } from "./utils/sendMessageWrapper";
 
+const onMessage = (messageId, callback) => _onMessage(messageId, callbackGuard(callback));
 const onWindowMessage = _onMessage;
+const sendMessageAsync = sendMessageWrapper(_sendMessage);
 
 export const backgroundMessenger = {
     onMessage,
     onWindowMessage,
-    sendMessage,
+    sendMessageAsync,
 };
