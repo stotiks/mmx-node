a944292231c4f93304e1c61aaf7bcb9835894861
stotiks <47013208+stotiks@users.noreply.github.com>
2025-04-24 13:58:32 +0300

    useNotificationMessageHandler

--- ui/src-bex/entrypoints/notification/main.js
+++ ui/src-bex/entrypoints/notification/main.js
@@ -1,9 +1,5 @@
-import { popupMessenger } from "@bex/messaging/popup";
-
-popupMessenger.onMessage("notification", async (message) => {
-    console.log("Received from background:", JSON.parse(JSON.stringify(message)));
-    // await new Promise((resolve) => setTimeout(resolve, 5000));
-    return { success: true, data: { message: "Return from notification" } };
-});
-
 console.log("Hello from notification world!");
+
+import { vueApp } from "../popup/vueApp.js";
+vueApp.provide("isNotification", true);
+vueApp.mount("#app");
