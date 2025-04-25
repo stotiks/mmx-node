675392b4ebe3246c6de96237304c84a0635098e2
stotiks <47013208+stotiks@users.noreply.github.com>
2025-04-25 19:59:54 +0300

    mmx_signTransaction

--- ui/src-bex/entrypoints/background/utils/walletHelpers.js
+++ ui/src-bex/entrypoints/background/utils/walletHelpers.js
@@ -1,7 +1,5 @@
-import { sha256 } from "@noble/hashes/sha2";
 import { bytesToHex } from "@noble/hashes/utils";
 import vault from "../storage/vault";
-import { spend_options_t } from "@/mmx/wallet/common/spend_options_t";
 
 const getWalletByAddress = async (address) => {
     return vault.getWallets().find((wallet) => wallet.address === address);
@@ -24,9 +22,8 @@ export const signMessageAsync = async (msg, address = vault.getCurrentWalletAddr
     return await ecdsaWallet.signMsgAsync(address, msg);
 };
 
-export const signTransactionAsync = async (tx, _options, address = vault.getCurrentWalletAddress()) => {
+export const signTransactionAsync = async (tx, options, address = vault.getCurrentWalletAddress()) => {
     const ecdsaWallet = await vault.getECDSAWalletAsync(address);
-    const options = new spend_options_t(_options);
     await ecdsaWallet.signOfAsync(tx, options);
     return tx;
 };
