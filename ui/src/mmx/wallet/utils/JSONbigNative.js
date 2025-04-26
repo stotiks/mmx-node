ba090a26a2475177cfe7e324af887f392df154b9
stotiks <47013208+stotiks@users.noreply.github.com>
2025-04-26 21:03:27 +0300

    Transaction.toJSON

--- ui/src/mmx/wallet/utils/JSONbigNative.js
+++ ui/src/mmx/wallet/utils/JSONbigNative.js
@@ -1,2 +1,3 @@
 import JSONbig from "json-bigint";
 export const JSONbigNative = JSONbig({ alwaysParseAsBig: false, useNativeBigInt: true });
+export const JSONbigNativeString = JSONbig({ alwaysParseAsBig: false, useNativeBigInt: true, storeAsString: true });
