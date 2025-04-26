46a3aabb75142d335090fc59dfb2e7ec59be5c4a
stotiks <47013208+stotiks@users.noreply.github.com>
2025-04-26 19:31:49 +0300

    ConfigBuilder.writeRobotsTxt

--- ui/vite.config.js
+++ ui/vite.config.js
@@ -3,6 +3,7 @@ import { ConfigBuilder, BuildTargets } from "./vite.ConfigBuilder";
 const configBuilder = new ConfigBuilder({
     buildTarget: BuildTargets.GUI,
     writeBuildInfo: true,
+    writeRobotsTxt: true,
 });
 
 export default configBuilder.config;
