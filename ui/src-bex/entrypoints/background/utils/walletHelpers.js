fad7eb59a486e67a0b836f0d2026c8c74c5115b4
stotiks <47013208+stotiks@users.noreply.github.com>
2025-04-29 15:54:31 +0300

    Error("No wallet selected")

--- ui/src-bex/entrypoints/background/utils/walletHelpers.js
+++ ui/src-bex/entrypoints/background/utils/walletHelpers.js
@@ -7,10 +7,16 @@ const getWalletByAddress = async (address) => {
 
 export const getCurrentWallet = () => {
     const address = vault.getCurrentWalletAddress();
+    if (address == null) {
+        throw new Error("No wallet selected");
+    }
     return getWalletByAddress(address);
 };
 
 export const getPubKeyAsync = async (address = vault.getCurrentWalletAddress()) => {
+    if (address == null) {
+        throw new Error("No wallet selected");
+    }
     const ecdsaWallet = await vault.getECDSAWalletAsync(address);
     const { pubKey } = await ecdsaWallet.getKeysAsync(0);
 
@@ -18,11 +24,17 @@ export const getPubKeyAsync = async (address = vault.getCurrentWalletAddress()) 
 };
 
 export const signMessageAsync = async (msg, address = vault.getCurrentWalletAddress()) => {
+    if (address == null) {
+        throw new Error("No wallet selected");
+    }
     const ecdsaWallet = await vault.getECDSAWalletAsync(address);
     return await ecdsaWallet.signMsgAsync(address, msg);
 };
 
 export const signTransactionAsync = async (tx, options, address = vault.getCurrentWalletAddress()) => {
+    if (address == null) {
+        throw new Error("No wallet selected");
+    }
     const ecdsaWallet = await vault.getECDSAWalletAsync(address);
     await ecdsaWallet.signOfAsync(tx, options);
 };
