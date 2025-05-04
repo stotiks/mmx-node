46f42712ce05849f87e92e27ec30d7f8f88e5b3e
stotiks <47013208+stotiks@users.noreply.github.com>
2025-05-04 15:08:51 +0300

    UnlockPage

---
+++ ui/src-bex/entrypoints/popup/components/dialogs/HandleRequestDialog/pages/UnlockPage.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="fullscreen row justify-center">
+        <q-card flat class="self-center col-xl-4 col-lg-6 col-md-8 col-sm-10 col-xs-10">
+            <q-toolbar>
+                <q-toolbar-title>
+                    <div class="row justify-center">Unlock</div>
+                </q-toolbar-title>
+            </q-toolbar>
+            <q-card-section>
+                <q-form class="q-gutter-sm" @submit="handleUnlockAsync">
+                    <q-input v-model="password" type="password" required filled label="Password">
+                        <template v-slot:prepend>
+                            <q-icon :name="mdiLock" />
+                        </template>
+                    </q-input>
+                    <div class="row justify-center">
+                        <q-btn
+                            label="Login"
+                            :icon="mdiLogin"
+                            type="submit"
+                            :color="password ? 'positive' : 'primary'"
+                            rounded
+                            outline
+                            :disable="!password"
+                        />
+                    </div>
+                </q-form>
+            </q-card-section>
+        </q-card>
+    </div>
+</template>
+
+<script setup>
+import { useTryCatchWrapper } from "@bex/entrypoints/popup/utils/useTryCatchWrapper";
+import { mdiLock, mdiLogin } from "@mdi/js";
+
+const password = ref("");
+
+import { useVaultStore } from "@bex/entrypoints/popup/stores/vault";
+const vaultStore = useVaultStore();
+
+const tryCatchWrapper = useTryCatchWrapper();
+const handleUnlockAsync = async () => {
+    await tryCatchWrapper(() => vaultStore.unlockAsync({ password: password.value }));
+};
+</script>
