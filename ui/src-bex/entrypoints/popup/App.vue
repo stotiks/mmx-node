26855f55ae38a37dcd801873dd81beef93746daa
stotiks <47013208+stotiks@users.noreply.github.com>
2025-05-09 00:24:26 +0300

    feat: implement MainPage component

--- ui/src-bex/entrypoints/popup/App.vue
+++ ui/src-bex/entrypoints/popup/App.vue
@@ -14,97 +14,59 @@
                 <template v-if="isUnlocked !== true">
                     <UnlockPage />
                 </template>
-                <q-page v-else padding style="padding-top: 66px">
-                    <div class="q-gutter-y-sm">
-                        <q-list bordered class="rounded-borders">
-                            <q-expansion-item
-                                label="Password update"
-                                header-class="text-h6"
-                                expand-separator
-                                group="expansionGroup"
-                            >
-                                <q-input v-model="password" type="password" label="Password" filled dense />
-                                <q-input v-model="newPassword" type="password" label="New password" filled dense />
-                                <q-btn @click="handleUpdatePasswordAsync">Update</q-btn>
-                            </q-expansion-item>
-
-                            <q-expansion-item
-                                label="Add wallet"
-                                header-class="text-h6"
-                                expand-separator
-                                group="expansionGroup"
-                            >
-                                <q-input v-model="newWalletMnemonic" label="Mnemonic" filled dense />
-                                <q-input v-model="newWalletPassword" label="Password" filled dense />
-                                <q-btn @click="handleAddWalletAsync">Add</q-btn>
-                            </q-expansion-item>
-                        </q-list>
-                        <WalletSelect2 />
-                        <q-btn
-                            :disabled="currentWalletAddress === ''"
-                            @click="handleRemoveWalletAsync(currentWalletAddress)"
-                        >
-                            Remove
-                        </q-btn>
-                    </div>
+                <template v-else>
+                    <MainPage style="padding-top: 66px" />
 
                     <q-page-sticky expand position="top">
-                        <q-toolbar class1="bg-primary text-white">
-                            <!-- <q-toolbar-title class="text-subtitle1"> </q-toolbar-title> -->
+                        <q-toolbar>
                             <q-space />
-                            <q-btn label="Lock" :icon="mdiLock" flat @click="handleLockAsync" />
+                            <q-btn flat :icon="mdiDotsVertical">
+                                <q-menu anchor="bottom left" self="top left">
+                                    <q-item clickable v-ripple @click="">
+                                        <q-item-section avatar>
+                                            <q-icon :name="mdiWallet" />
+                                        </q-item-section>
+                                        <q-item-section>Wallets</q-item-section>
+                                    </q-item>
+                                    <q-item clickable v-ripple @click="">
+                                        <q-item-section avatar>
+                                            <q-icon :name="mdiShieldLock" />
+                                        </q-item-section>
+                                        <q-item-section>Password</q-item-section>
+                                    </q-item>
+                                    <q-separator />
+                                    <q-item clickable v-ripple @click="handleLockAsync">
+                                        <q-item-section avatar>
+                                            <q-icon :name="mdiLock" />
+                                        </q-item-section>
+                                        <q-item-section>Lock</q-item-section>
+                                    </q-item>
+                                </q-menu>
+                            </q-btn>
                         </q-toolbar>
                     </q-page-sticky>
-                </q-page>
+                </template>
             </template>
         </q-page-container>
     </q-layout>
 </template>
 
 <script setup>
+import { mdiAlphaFBox, mdiLock, mdiDotsVertical, mdiWallet, mdiShieldLock } from "@mdi/js";
+
+import MainPage from "@bex/entrypoints/popup/components/dialogs/HandleRequestDialog/pages/MainPage";
 import UnlockPage from "@bex/entrypoints/popup/components/UnlockPage";
-import WalletSelect2 from "@bex/entrypoints/popup/components/WalletSelect2";
-
-import { mdiAlphaFBox, mdiLock } from "@mdi/js";
-
-const password = ref("password");
-const newPassword = ref("password");
 
 import { useTryCatchWrapperAsync } from "./utils/useTryCatchWrapperAsync";
 const tryCatchWrapperAsync = useTryCatchWrapperAsync();
 
 import { useVaultStore } from "@bex/entrypoints/popup/stores/vault";
 const vaultStore = useVaultStore();
-const { isUnlocked, currentWalletAddress } = storeToRefs(vaultStore);
-
-const handleUnlockAsync = async () => {
-    await tryCatchWrapperAsync(() => vaultStore.unlockAsync({ password: password.value }));
-};
+const { isUnlocked } = storeToRefs(vaultStore);
 
 const handleLockAsync = async () => {
     await tryCatchWrapperAsync(() => vaultStore.lockAsync());
 };
-
-const handleUpdatePasswordAsync = async () => {
-    await tryCatchWrapperAsync(() =>
-        vaultStore.updatePasswordAsync({ password: password.value, newPassword: newPassword.value })
-    );
-};
-
-const newWalletMnemonic = ref("");
-const newWalletPassword = ref("");
-const handleAddWalletAsync = async () => {
-    await tryCatchWrapperAsync(() =>
-        vaultStore.addWalletAsync({ mnemonic: newWalletMnemonic.value, password: newWalletPassword.value })
-    );
-};
-
-const handleRemoveWalletAsync = async (address) => {
-    await tryCatchWrapperAsync(() => vaultStore.removeWalletAsync({ address }));
-};
-
-import { useVaultMessageHandler } from "@bex/entrypoints/popup/MessageHandlers/useVaultMessageHandler";
-useVaultMessageHandler();
 
 import { useNotificationMessageHandler } from "./MessageHandlers/useNotificationMessageHandler";
 const { isLoading } = useNotificationMessageHandler();
