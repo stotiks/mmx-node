ed395a8171c30a171798d0402e3bf88e186ac1d1
stotiks <47013208+stotiks@users.noreply.github.com>
2025-05-09 00:42:59 +0300

    feat: add MainPage and UnlockPage components with routing support

--- ui/src-bex/entrypoints/popup/plugins.js
+++ ui/src-bex/entrypoints/popup/plugins.js
@@ -3,7 +3,7 @@ import { Quasar, quasarConfig } from "@/plugins/quasar";
 // import pinia from "@/plugins/pinia";
 ////import router from "@/plugins/router";
 import i18n from "@/plugins/i18n";
-// import VueQueryPlugin, { vueQueryPluginOptions } from "@/plugins/query";
+import VueQueryPlugin, { vueQueryPluginOptions } from "@/plugins/query";
 import highlight from "@/plugins/highlight";
 
 import { createPinia } from "pinia";
@@ -18,13 +18,21 @@ pinia.use(({ store }) => {
     // store.queryClient = markRaw(useQueryClient());
 });
 
-export default pinia;
+// export default pinia;
+
+import { createRouter, createWebHashHistory } from "vue-router";
+import routes from "./routes";
+
+const router = createRouter({
+    history: createWebHashHistory(import.meta.env.BASE_URL),
+    routes: routes,
+});
 
 export function registerPlugins(app) {
     app.use(Quasar, quasarConfig);
-    //app.use(router);
+    app.use(router);
     app.use(pinia);
     app.use(i18n);
-    // app.use(VueQueryPlugin, vueQueryPluginOptions);
+    app.use(VueQueryPlugin, vueQueryPluginOptions);
     app.use(highlight);
 }
