b8135ac98a216e2af7b523601370fd06b93b303d
stotiks <47013208+stotiks@users.noreply.github.com>
2025-05-09 01:09:46 +0300

    feat: add PasswordPage and WalletsPage components with routing support

--- ui/src-bex/entrypoints/popup/router/routes.js
+++ ui/src-bex/entrypoints/popup/router/routes.js
@@ -3,4 +3,14 @@ export default [
         path: "/",
         component: () => import("@bex/entrypoints/popup/pages/MainPage"),
     },
+
+    {
+        path: "/password",
+        component: () => import("@bex/entrypoints/popup/pages/PasswordPage"),
+    },
+
+    {
+        path: "/wallets",
+        component: () => import("@bex/entrypoints/popup/pages/WalletsPage"),
+    },
 ];
