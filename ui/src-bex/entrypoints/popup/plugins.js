508361a5a8e6c95e1b58db7d2ddecff6bf226d0d
stotiks <47013208+stotiks@users.noreply.github.com>
2025-05-09 00:47:01 +0300

    Refactor router setup: Move router creation to a dedicated file and define routes in a separate module for better organization and maintainability.

--- ui/src-bex/entrypoints/popup/plugins.js
+++ ui/src-bex/entrypoints/popup/plugins.js
@@ -1,32 +1,19 @@
 // Plugins
 import { Quasar, quasarConfig } from "@/plugins/quasar";
 // import pinia from "@/plugins/pinia";
-////import router from "@/plugins/router";
+import router from "./router";
 import i18n from "@/plugins/i18n";
 import VueQueryPlugin, { vueQueryPluginOptions } from "@/plugins/query";
 import highlight from "@/plugins/highlight";
 
 import { createPinia } from "pinia";
-
-// import router from "@/plugins/router";
-// import { useQueryClient } from "@tanstack/vue-query";
-
 const pinia = createPinia();
-
 pinia.use(({ store }) => {
     // store.router = markRaw(router);
     // store.queryClient = markRaw(useQueryClient());
 });
 
 // export default pinia;
-
-import { createRouter, createWebHashHistory } from "vue-router";
-import routes from "./routes";
-
-const router = createRouter({
-    history: createWebHashHistory(import.meta.env.BASE_URL),
-    routes: routes,
-});
 
 export function registerPlugins(app) {
     app.use(Quasar, quasarConfig);
