4cea20deb7195c06292f1819a1e891fd427d23b2
stotiks <47013208+stotiks@users.noreply.github.com>
2025-05-25 20:41:41 +0300

    refactor: reorganize notification message handler logic for clarity

--- ui/src-bex/entrypoints/popup/MessageHandlers/useNotificationMessageHandler.js
+++ ui/src-bex/entrypoints/popup/MessageHandlers/useNotificationMessageHandler.js
@@ -2,63 +2,59 @@ import { popupMessenger } from "@bex/messaging/popup";
 import { MessageHandlerBase } from "@bex/messaging/utils/MessageHandlerBase";
 
 export const useNotificationMessageHandler = () => {
-    const isNotification = inject("isNotification");
-
     const isRunning = ref(false);
     const isLoading = ref(true);
 
-    if (!isNotification) {
-        isLoading.value = false;
-        return { isRunning, isLoading };
-    }
+    const isNotification = inject("isNotification");
+    if (isNotification) {
+        const $q = useQuasar();
 
-    const $q = useQuasar();
-
-    const showHandleRequestDialogAsync = (props) => {
-        return new Promise((resolve) => {
-            $q.dialog({
-                component: defineAsyncComponent(
-                    () => import("@bex/entrypoints/popup/components/dialogs/HandleRequestDialog")
-                ),
-                componentProps: props,
-            })
-                .onOk((data) => {
-                    resolve(data);
+        const showHandleRequestDialogAsync = (props) => {
+            return new Promise((resolve) => {
+                $q.dialog({
+                    component: defineAsyncComponent(
+                        () => import("@bex/entrypoints/popup/components/dialogs/HandleRequestDialog")
+                    ),
+                    componentProps: props,
                 })
-                .onCancel((data) => {
-                    resolve(data);
-                });
-        });
-    };
-
-    class NotificationMessageHandlerMethods {
-        static dummy = () => {};
-
-        static requestPermissionsAndAccept = async (params) => {
-            if (isRunning.value === true) {
-                throw new Error("Other request is running");
-            }
-
-            try {
-                isRunning.value = true;
-                const data = await showHandleRequestDialogAsync(params).catch(() => false);
-                return { success: true, data };
-            } finally {
-                isRunning.value = false;
-            }
-        };
-    }
-
-    class MessageHandlerNotification extends MessageHandlerBase {
-        async handleAsync(message) {
-            return await super.handleAsync(message).finally(() => {
-                isLoading.value = false;
+                    .onOk((data) => {
+                        resolve(data);
+                    })
+                    .onCancel((data) => {
+                        resolve(data);
+                    });
             });
-        }
-    }
+        };
 
-    const notificationMessageHandler = new MessageHandlerNotification(NotificationMessageHandlerMethods);
-    notificationMessageHandler.register(popupMessenger.onMessage, "notification");
+        class NotificationMessageHandlerMethods {
+            static requestPermissionsAndAccept = async (params) => {
+                if (isRunning.value === true) {
+                    throw new Error("Other request is running");
+                }
+
+                try {
+                    isRunning.value = true;
+                    const data = await showHandleRequestDialogAsync(params).catch(() => false);
+                    return { success: true, data };
+                } finally {
+                    isRunning.value = false;
+                }
+            };
+        }
+
+        class MessageHandlerNotification extends MessageHandlerBase {
+            async handleAsync(message) {
+                return await super.handleAsync(message).finally(() => {
+                    isLoading.value = false;
+                });
+            }
+        }
+
+        const notificationMessageHandler = new MessageHandlerNotification(NotificationMessageHandlerMethods);
+        notificationMessageHandler.register(popupMessenger.onMessage, "notification");
+    } else {
+        isLoading.value = false;
+    }
 
     return { isRunning, isLoading };
 };
