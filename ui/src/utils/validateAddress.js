14792cd284f5527b23e34bf14c60f9dea0dd2253
stotiks <47013208+stotiks@users.noreply.github.com>
2025-05-26 03:03:21 +0300

    fix: enhance address validation to check prefix before word length

--- ui/src/utils/validateAddress.js
+++ ui/src/utils/validateAddress.js
@@ -2,8 +2,12 @@ import { bech32m } from "@scure/base";
 
 export const validateAddress = (address) => {
     try {
-        const decoded = bech32m.decode(address);
-        if (decoded.words.length != 52) {
+        const { prefix, words } = bech32m.decode(address);
+        if (prefix !== "mmx") {
+            // invalid HRP
+            return false;
+        }
+        if (words.length != 52) {
             // (size != 52)
             return false;
         }
