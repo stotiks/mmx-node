9091dfe80b5040e61940c13f1347f4fbc169ca7c
stotiks <47013208+stotiks@users.noreply.github.com>
2025-05-30 01:39:40 +0300

    fix: update getNodeInfo to getNodeInfoAsync for consistency and add params support in getData

--- ui/src-bex/entrypoints/background/queries.js
+++ ui/src-bex/entrypoints/background/queries.js
@@ -1,7 +1,9 @@
-const getData = (endpoint) => {
+const getData = (endpoint, params) => {
     // eslint-disable-next-line no-undef
     const url = new URL(endpoint, __PUBLIC_RPC_URL__);
-    return fetch(url).then((res) => res.json());
+    return fetch(url + `?${new URLSearchParams(params)}`).then((res) => res.json());
 };
 
-export const getNodeInfo = () => getData("/node/info");
+export const getNodeInfoAsync = () => getData("/node/info");
+
+export const getContractAsync = (address) => getData("/contract", { id: address });
