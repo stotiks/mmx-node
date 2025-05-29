8ff7bb9798d80b5beac653b0ad750cadb6a9a44a
stotiks <47013208+stotiks@users.noreply.github.com>
2025-05-29 23:09:01 +0300

    fix: restructure WalletsPage.vue to enhance wallet management interface

--- ui/src-bex/entrypoints/popup/pages/WalletsPage.vue
+++ ui/src-bex/entrypoints/popup/pages/WalletsPage.vue
@@ -1,7 +1,15 @@
 <template>
     <q-page padding>
         <div class="q-gutter-y-sm">
-            <q-card flat>
+            <q-table :rows="wallets" :columns="columns" :pagination="{ rowsPerPage: 0 }" hide-pagination hide-header>
+                <template v-slot:body-cell-actions="bcProps">
+                    <q-td :props="bcProps">
+                        <q-btn flat :icon="mdiDelete" @click="handleRemoveWalletAsync(bcProps.row.address)" />
+                    </q-td>
+                </template>
+            </q-table>
+
+            <q-card flat class="q-mt-md">
                 <q-card-section class="q-gutter-y-sm">
                     <div>Add wallet</div>
                     <q-input v-model="newWalletMnemonic" label="Mnemonic" filled dense />
@@ -9,31 +17,22 @@
                     <q-btn flat @click="handleAddWalletAsync">Add</q-btn>
                 </q-card-section>
             </q-card>
-            <q-card flat>
-                <q-card-section class="q-gutter-y-sm">
-                    <WalletSelect2 />
-                    <q-btn
-                        flat
-                        :disabled="currentWalletAddress === ''"
-                        @click="handleRemoveWalletAsync(currentWalletAddress)"
-                    >
-                        Remove
-                    </q-btn>
-                </q-card-section>
-            </q-card>
         </div>
     </q-page>
 </template>
 
 <script setup>
-import WalletSelect2 from "@bex/entrypoints/popup/components/WalletSelect2";
-
+import { mdiDelete } from "@mdi/js";
 import { useTryCatchWrapperAsync } from "@bex/entrypoints/popup/utils/useTryCatchWrapperAsync";
 const tryCatchWrapperAsync = useTryCatchWrapperAsync();
 
 import { useVaultStore } from "@bex/entrypoints/popup/stores/vault";
 const vaultStore = useVaultStore();
-const { currentWalletAddress } = storeToRefs(vaultStore);
+const { wallets, currentWalletAddress } = storeToRefs(vaultStore);
+const columns = computed(() => [
+    { name: "height", label: "Wallet", field: "address", align: "left" },
+    { name: "actions", label: "", align: "left" },
+]);
 
 const newWalletMnemonic = ref("");
 const newWalletPassword = ref("");
