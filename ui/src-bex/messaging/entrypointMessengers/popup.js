72eb8c77cebb21ed123de09cc9d20f90f93b8f21
stotiks <47013208+stotiks@users.noreply.github.com>
2025-06-23 21:12:18 +0300

    fix: refactor messaging imports to use entrypointMessengers for better organization

--- ui/src-bex/messaging/entrypointMessengers/popup.js
+++ ui/src-bex/messaging/entrypointMessengers/popup.js
@@ -1,5 +1,5 @@
 // eslint-disable-next-line no-restricted-imports
 import * as messenger from "webext-bridge/popup";
-import { messengerWrapper } from "./utils/messengerWrapper";
+import { messengerWrapper } from "../utils/messengerWrapper";
 
 export const popupMessenger = messengerWrapper(messenger);
--- ui/src-bex/messaging/entrypointMessengers/window.js
+++ ui/src-bex/messaging/entrypointMessengers/window.js
@@ -1,5 +1,5 @@
 // eslint-disable-next-line no-restricted-imports
 import * as messenger from "webext-bridge/window";
-import { messengerWrapper } from "./utils/messengerWrapper";
+import { messengerWrapper } from "../utils/messengerWrapper";
 
 export const windowMessenger = messengerWrapper(messenger);
