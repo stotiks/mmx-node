20aeffe41d6708c4c44dcedeb4b6da9d7f50bf6e
stotiks <47013208+stotiks@users.noreply.github.com>
2025-06-24 19:02:39 +0300

    refactor: enhance wallet address display in WalletSelect2 and WalletsPage components

--- ui/src-bex/entrypoints/popup/components/WalletSelect2.vue
+++ ui/src-bex/entrypoints/popup/components/WalletSelect2.vue
@@ -2,6 +2,7 @@
     <q-select
         v-model="currentWalletAddress"
         :options="walletsOptions"
+        :display-value="currentWalletAddress ? getShortAddr(currentWalletAddress, 25) : ''"
         emit-value
         map-options
         label="Wallet"
@@ -16,6 +17,9 @@ const vaultStore = useVaultStore();
 const { wallets, currentWalletAddress } = storeToRefs(vaultStore);
 
 const walletsOptions = computed(() =>
-    wallets.value.map((wallet) => ({ label: wallet.address, value: wallet.address }))
+    wallets.value.map((wallet) => ({
+        label: getShortAddr(wallet.address, 25),
+        value: wallet.address,
+    }))
 );
 </script>
