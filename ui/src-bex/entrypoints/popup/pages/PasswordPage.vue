73d7f0e42ebf216e839fb373e48a2e592e7ab2e2
stotiks <47013208+stotiks@users.noreply.github.com>
2025-06-24 17:10:03 +0300

    refactor: enhance password validation rules in PasswordPage and remove legacy password rule logic

--- ui/src-bex/entrypoints/popup/pages/PasswordPage.vue
+++ ui/src-bex/entrypoints/popup/pages/PasswordPage.vue
@@ -13,13 +13,26 @@
                         :rules="[rules.required]"
                     />
 
+                    <div class="text-caption">
+                        Password must contain at least:
+                        <ul style="list-style-type: none; padding-left: 0">
+                            <li
+                                v-for="(rule, index) in passwordRules"
+                                :key="index"
+                                :class="{ 'text-positive': rule.valid }"
+                            >
+                                <q-icon :name="rule.valid ? mdiCheck : mdiClose" />
+                                {{ rule.label }}
+                            </li>
+                        </ul>
+                    </div>
                     <WPasswordInput
                         v-model="newPassword"
                         label="New password"
                         filled
                         dense
                         hide-bottom-space
-                        :rules="[rules.required, rules.password]"
+                        :rules="[rules.required, passwordRule]"
                         class="q-mt-md"
                     />
                     <WPasswordInput
@@ -42,6 +55,7 @@
 </template>
 
 <script setup>
+import { mdiCheck, mdiClose } from "@mdi/js";
 import WPasswordInput from "@/components/UI/WPasswordInput.vue";
 
 import rules from "@/helpers/rules";
@@ -51,13 +65,52 @@ import { useVaultStore } from "@bex/entrypoints/popup/stores/vault";
 const vaultStore = useVaultStore();
 
 import { useTryCatchWrapperAsync } from "@bex/entrypoints/popup/utils/useTryCatchWrapperAsync";
-
 const tryCatchWrapperAsync = useTryCatchWrapperAsync();
 
 const test_password = process.env.NODE_ENV === "development" && import.meta.env.VITE_TEST_PASSWORD;
 const password = ref(test_password || "");
 const newPassword = ref(test_password || "");
 const newPasswordConfirm = ref(test_password || "");
+
+const passwordRules = computed(() => {
+    const value = newPassword.value;
+    return [
+        {
+            label: "12 characters",
+            valid: value.length >= 12,
+            message: "Password must be at least 12 characters long",
+        },
+        {
+            label: "one digit",
+            valid: /\d/.test(value),
+            message: "Password must contain at least one digit",
+        },
+        {
+            label: "one lowercase letter",
+            valid: /[a-z]/.test(value),
+            message: "Password must contain at least one lowercase letter",
+        },
+        {
+            label: "one uppercase letter",
+            valid: /[A-Z]/.test(value),
+            message: "Password must contain at least one uppercase letter",
+        },
+        {
+            label: "one special character",
+            valid: /[!@#$%^&*(),.?":{}|<>]/.test(value),
+            message: "Password must contain at least one special character",
+        },
+    ];
+});
+
+const passwordRule = (value) => {
+    for (const rule of passwordRules.value) {
+        if (!rule.valid) {
+            return rule.message;
+        }
+    }
+    return true;
+};
 
 const handleUpdatePasswordAsync = async () => {
     await tryCatchWrapperAsync(() =>
