a6fedcbe11d0b8245aa29fdae84e3d46de944f97
stotiks <47013208+stotiks@users.noreply.github.com>
2025-06-24 15:36:14 +0300

    password rule

--- ui/src/helpers/rules.js
+++ ui/src/helpers/rules.js
@@ -6,16 +6,35 @@ const rules = {
     address: (value) => !value || validateAddress(value) || "Invalid address", //TODO i18n
 
     // is non empty
-    required: (value) => !isEmpty(value) || "Field is required",
+    required: (value) => !isEmpty(value) || "Field is required", //TODO i18n
 
     amount: (value) => {
         //if (value && value.length && value.match(/^(\d+([.,]\d*)?)$/)) {
-        return isEmpty(value) || (typeof value === "number" && value > 0) || "Invalid amount";
+        return isEmpty(value) || (typeof value === "number" && value > 0) || "Invalid amount"; //TODO i18n
     },
 
     memo: (value) => {
         if (value && value.length > 64) {
-            return "Maximum length is 64";
+            return "Maximum length is 64"; //TODO i18n
+        }
+        return true;
+    },
+
+    password: (value) => {
+        if (value.length < 12) {
+            return "Password must be at least 12 characters long"; //TODO i18n
+        }
+        if (!/\d/.test(value)) {
+            return "Password must contain at least one digit"; //TODO i18n
+        }
+        if (!/[a-z]/.test(value)) {
+            return "Password must contain at least one lowercase letter"; //TODO i18n
+        }
+        if (!/[A-Z]/.test(value)) {
+            return "Password must contain at least one uppercase letter"; //TODO i18n
+        }
+        if (!/[!@#$%^&*(),.?":{}|<>]/.test(value)) {
+            return "Password must contain at least one special character"; //TODO i18n
         }
         return true;
     },
