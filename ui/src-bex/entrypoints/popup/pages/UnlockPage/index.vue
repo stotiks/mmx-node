b47fe3713ef355e6b31c87668691710669e55a3c
stotiks <47013208+stotiks@users.noreply.github.com>
2025-06-28 01:02:13 +0300

    feat: add UnlockForm component to UnlockPage for user authentication

--- ui/src-bex/entrypoints/popup/pages/UnlockPage/index.vue
+++ ui/src-bex/entrypoints/popup/pages/UnlockPage/index.vue
@@ -2,11 +2,6 @@
     <q-page padding style="padding-top: 66px">
         <div class="fullscreen row justify-center">
             <q-card flat class="self-center col-xl-4 col-lg-6 col-md-8 col-sm-10 col-xs-10">
-                <!-- <q-toolbar>
-                    <q-toolbar-title>
-                        <div class="row justify-center">Unlock</div>
-                    </q-toolbar-title>
-                </q-toolbar> -->
                 <q-card-section>
                     <UnlockForm />
                 </q-card-section>
