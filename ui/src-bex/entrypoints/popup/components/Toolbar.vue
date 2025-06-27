6c733fa4cfecbd6c03f820d21e84a4fe0b41d0a0
stotiks <47013208+stotiks@users.noreply.github.com>
2025-06-28 00:36:43 +0300

    feat: create Toolbar component for improved navigation and vault management

---
+++ ui/src-bex/entrypoints/popup/components/Toolbar.vue
@@ -0,0 +1,73 @@
+<template>
+    <q-page-sticky expand position="top">
+        <q-toolbar>
+            <template v-if="route.path !== '/'">
+                <q-btn flat :icon="mdiArrowLeft" :to="{ name: 'home' }" />
+            </template>
+            <q-space />
+            <q-btn flat :icon="mdiDotsVertical">
+                <q-menu anchor="bottom left" self="top left">
+                    <q-item v-ripple clickable to="/wallets">
+                        <q-item-section avatar>
+                            <q-icon :name="mdiWallet" />
+                        </q-item-section>
+                        <q-item-section>Wallets</q-item-section>
+                    </q-item>
+                    <q-item v-ripple clickable to="/password">
+                        <q-item-section avatar>
+                            <q-icon :name="mdiShieldLock" />
+                        </q-item-section>
+                        <q-item-section>Password</q-item-section>
+                    </q-item>
+                    <q-separator />
+                    <q-item v-ripple clickable @click="handleLockAsync">
+                        <q-item-section avatar>
+                            <q-icon :name="mdiLock" />
+                        </q-item-section>
+                        <q-item-section>Lock</q-item-section>
+                    </q-item>
+                    <q-separator />
+                    <q-item v-ripple clickable @click="handleRemoveVaultDataAsync">
+                        <q-item-section avatar>
+                            <q-icon :name="mdiTrashCanOutline" color="negative" />
+                        </q-item-section>
+                        <q-item-section class="text-negative text-no-wrap"> Remove Vault Data </q-item-section>
+                    </q-item>
+                </q-menu>
+            </q-btn>
+        </q-toolbar>
+    </q-page-sticky>
+</template>
+
+<script setup>
+import { mdiLock, mdiDotsVertical, mdiWallet, mdiShieldLock, mdiArrowLeft, mdiTrashCanOutline } from "@mdi/js";
+
+const route = useRoute();
+
+import { useTryCatchWrapperAsync } from "../utils/useTryCatchWrapperAsync";
+const tryCatchWrapperAsync = useTryCatchWrapperAsync();
+
+import { useVaultStore } from "../stores/vault";
+const vaultStore = useVaultStore();
+
+const handleLockAsync = async () => {
+    await tryCatchWrapperAsync(() => vaultStore.lockAsync());
+};
+
+const $q = useQuasar();
+const handleRemoveVaultDataAsync = async () => {
+    await tryCatchWrapperAsync(async () => {
+        $q.dialog({
+            title: "Confirm",
+            message: "Are you sure you want to remove the vault data? This action cannot be undone.",
+            cancel: true,
+            persistent: true,
+            ok: {
+                color: "negative",
+            },
+        }).onOk(async () => {
+            await vaultStore.removeVaultDataAsync();
+        });
+    });
+};
+</script>
