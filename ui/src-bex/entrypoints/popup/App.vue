85b1420f9065b745721551727c3c4d21184d4cb2
stotiks <47013208+stotiks@users.noreply.github.com>
2025-06-28 00:36:43 +0300

    feat: create Toolbar component for improved navigation and vault management

--- ui/src-bex/entrypoints/popup/App.vue
+++ ui/src-bex/entrypoints/popup/App.vue
@@ -14,46 +14,7 @@
                     <q-inner-loading :showing="isActionRunning">
                         <q-spinner-gears size="50px" color="primary" />
                     </q-inner-loading>
-                    <q-page-sticky expand position="top">
-                        <q-toolbar>
-                            <template v-if="route.path !== '/'">
-                                <q-btn flat :icon="mdiArrowLeft" :to="{ name: 'home' }" />
-                            </template>
-                            <q-space />
-                            <q-btn flat :icon="mdiDotsVertical">
-                                <q-menu anchor="bottom left" self="top left">
-                                    <q-item v-ripple clickable to="/wallets">
-                                        <q-item-section avatar>
-                                            <q-icon :name="mdiWallet" />
-                                        </q-item-section>
-                                        <q-item-section>Wallets</q-item-section>
-                                    </q-item>
-                                    <q-item v-ripple clickable to="/password">
-                                        <q-item-section avatar>
-                                            <q-icon :name="mdiShieldLock" />
-                                        </q-item-section>
-                                        <q-item-section>Password</q-item-section>
-                                    </q-item>
-                                    <q-separator />
-                                    <q-item v-ripple clickable @click="handleLockAsync">
-                                        <q-item-section avatar>
-                                            <q-icon :name="mdiLock" />
-                                        </q-item-section>
-                                        <q-item-section>Lock</q-item-section>
-                                    </q-item>
-                                    <q-separator />
-                                    <q-item v-ripple clickable @click="handleRemoveVaultDataAsync">
-                                        <q-item-section avatar>
-                                            <q-icon :name="mdiTrashCanOutline" color="negative" />
-                                        </q-item-section>
-                                        <q-item-section class="text-negative text-no-wrap">
-                                            Remove Vault Data
-                                        </q-item-section>
-                                    </q-item>
-                                </q-menu>
-                            </q-btn>
-                        </q-toolbar>
-                    </q-page-sticky>
+                    <Toolbar />
                 </template>
             </template>
         </q-page-container>
@@ -63,64 +24,18 @@
 </template>
 
 <script setup>
-import {
-    mdiAlphaFBox,
-    mdiLock,
-    mdiDotsVertical,
-    mdiWallet,
-    mdiShieldLock,
-    mdiArrowLeft,
-    mdiTrashCanOutline,
-} from "@mdi/js";
 import { VueQueryDevtools } from "@tanstack/vue-query-devtools";
 
-import { useRoute } from "vue-router";
-const route = useRoute();
-
 import UnlockPage from "@bex/entrypoints/popup/pages/UnlockPage";
-
-import { useTryCatchWrapperAsync } from "./utils/useTryCatchWrapperAsync";
-const tryCatchWrapperAsync = useTryCatchWrapperAsync();
+import Toolbar from "./components/Toolbar.vue";
 
 import { useVaultStore } from "@bex/entrypoints/popup/stores/vault";
 const vaultStore = useVaultStore();
 const { isInitialized, isUnlocked } = storeToRefs(vaultStore);
-
-const handleLockAsync = async () => {
-    await tryCatchWrapperAsync(() => vaultStore.lockAsync());
-};
-
-const $q = useQuasar();
-const handleRemoveVaultDataAsync = async () => {
-    await tryCatchWrapperAsync(async () => {
-        $q.dialog({
-            title: "Confirm",
-            message: "Are you sure you want to remove the vault data? This action cannot be undone.",
-            cancel: true,
-            persistent: true,
-            ok: {
-                color: "negative",
-            },
-        }).onOk(async () => {
-            await vaultStore.removeVaultDataAsync();
-        });
-    });
-};
 
 import { useVaultMessageHandler } from "@bex/entrypoints/popup/MessageHandlers/useVaultMessageHandler";
 useVaultMessageHandler();
 
 import { useNotificationMessageHandler } from "./MessageHandlers/useNotificationMessageHandler";
 const { isMounted } = useNotificationMessageHandler();
-
-const isActionRunning = ref(false);
-vaultStore.$onAction(({ name, after, onError }) => {
-    isActionRunning.value = true;
-    after(() => {
-        isActionRunning.value = false;
-    });
-    onError(() => {
-        isActionRunning.value = false;
-    });
-});
 </script>
