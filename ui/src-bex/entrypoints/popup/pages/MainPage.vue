b10fbdc7ffde3c380211613dfba5226cbe172f6a
stotiks <47013208+stotiks@users.noreply.github.com>
2025-07-04 18:34:54 +0300

    feat: enhance BalanceTable with conditional RouterLink rendering and update props for better contract handling

--- ui/src-bex/entrypoints/popup/pages/MainPage.vue
+++ ui/src-bex/entrypoints/popup/pages/MainPage.vue
@@ -1,9 +1,16 @@
 <template>
     <q-page padding>
-        <WalletSelect2 />
+        <div class="q-gutter-y-md">
+            <WalletSelect2 />
+            <BalanceTable :address="currentWalletAddress" dense use-short-addr :use-contract-link="false" show-empty />
+        </div>
     </q-page>
 </template>
 
 <script setup>
 import WalletSelect2 from "@bex/entrypoints/popup/components/WalletSelect2";
+
+import { useVaultStore } from "@bex/entrypoints/popup/stores/vault";
+const vaultStore = useVaultStore();
+const { currentWalletAddress } = storeToRefs(vaultStore);
 </script>
