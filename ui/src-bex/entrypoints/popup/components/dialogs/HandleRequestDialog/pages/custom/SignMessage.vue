fdcc1c5c2bde209670f57f942f76e31fef6a61e1
stotiks <47013208+stotiks@users.noreply.github.com>
2025-07-04 23:01:31 +0300

    feat: add SignMessage component for displaying message to sign in AcceptPage

---
+++ ui/src-bex/entrypoints/popup/components/dialogs/HandleRequestDialog/pages/custom/SignMessage.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <p class="text-subtitle1">Message to sign:</p>
+        <q-input :model-value="params.message" type="textarea" readonly autogrow outlined class="message" />
+    </div>
+</template>
+
+<script setup>
+defineProps({
+    params: {
+        type: Object,
+        required: true,
+    },
+});
+</script>
+
+<style lang="scss" scoped>
+:deep(.message textarea) {
+    font-family: monospace !important;
+    color: $positive !important;
+}
+</style>
