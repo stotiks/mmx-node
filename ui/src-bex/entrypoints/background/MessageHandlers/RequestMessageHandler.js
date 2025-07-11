87e3c11c70a200d7b8f3515ef91d2d2ef603ca4a
stotiks <47013208+stotiks@users.noreply.github.com>
2025-07-11 22:45:48 +0300

    feat: add createHistoryHook to manage history in MessageHandler

--- ui/src-bex/entrypoints/background/MessageHandlers/RequestMessageHandler.js
+++ ui/src-bex/entrypoints/background/MessageHandlers/RequestMessageHandler.js
@@ -1,7 +1,9 @@
 import { MessageHandler } from "@bex/messaging/MessageHandler";
 import { requestMessageHandlerMethods } from "./requestMessageHandlerMethods";
 import { createAuthHook } from "./createAuthHook";
+import { createHistoryHook } from "./createHistoryHook";
 
 const requestMessageHandler = new MessageHandler(requestMessageHandlerMethods);
 requestMessageHandler.addHook(createAuthHook());
+requestMessageHandler.addAfterHook(createHistoryHook());
 export { requestMessageHandler };
