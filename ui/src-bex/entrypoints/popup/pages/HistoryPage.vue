e781bc2b83fbae18dccbbfb473edb00c84fbab0f
stotiks <47013208+stotiks@users.noreply.github.com>
2025-07-12 01:44:45 +0300

    design

--- ui/src-bex/entrypoints/popup/pages/HistoryPage.vue
+++ ui/src-bex/entrypoints/popup/pages/HistoryPage.vue
@@ -1,12 +1,22 @@
 <template>
-    <q-page padding style="padding-top: 66px">
-        <h1 class="text-h5">History</h1>
-        <q-card flat>
+    <q-page padding>
+        <!-- <h1 class="text-h5">History</h1> -->
+        <q-card flat class="q-mx-md">
             <q-list bordered separator>
                 <q-item v-for="(item, index) in history" :key="index">
                     <q-item-section>
                         <q-item-label>{{ item.method }}</q-item-label>
                         <q-item-label caption>{{ new Date(item.time).toLocaleString() }}</q-item-label>
+                    </q-item-section>
+                    <q-item-section v-if="item.result" side>
+                        <q-icon
+                            :name="item.result.success ? mdiCheckCircle : mdiAlertCircle"
+                            :color="item.result.success ? 'positive' : 'negative'"
+                        >
+                            <q-tooltip>
+                                {{ item.result.success ? "Success" : "Failed" }}
+                            </q-tooltip>
+                        </q-icon>
                     </q-item-section>
                 </q-item>
                 <q-item v-if="!history || !history.length">
@@ -20,6 +30,7 @@
 </template>
 
 <script setup>
+import { mdiCheckCircle, mdiAlertCircle } from "@mdi/js";
 import { useVaultStore } from "@bex/entrypoints/popup/stores/vault";
 const vaultStore = useVaultStore();
 const { history } = storeToRefs(vaultStore);
