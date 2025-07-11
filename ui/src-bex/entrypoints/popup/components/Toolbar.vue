c4ba700b40f2ed39be2448c2fa97d4b02934423f
stotiks <47013208+stotiks@users.noreply.github.com>
2025-07-12 00:54:04 +0300

    dynamic toolbar

--- ui/src-bex/entrypoints/popup/components/Toolbar.vue
+++ ui/src-bex/entrypoints/popup/components/Toolbar.vue
@@ -7,38 +7,19 @@
             <q-space />
             <q-btn flat :icon="mdiDotsVertical">
                 <q-menu anchor="bottom left" self="top left">
-                    <q-item v-ripple clickable to="/wallets">
-                        <q-item-section avatar>
-                            <q-icon :name="mdiWallet" />
-                        </q-item-section>
-                        <q-item-section>Wallets</q-item-section>
-                    </q-item>
-                    <q-item v-ripple clickable to="/history">
-                        <q-item-section avatar>
-                            <q-icon :name="mdiHistory" />
-                        </q-item-section>
-                        <q-item-section>History</q-item-section>
-                    </q-item>
-                    <q-item v-ripple clickable to="/password">
-                        <q-item-section avatar>
-                            <q-icon :name="mdiShieldLock" />
-                        </q-item-section>
-                        <q-item-section>Password</q-item-section>
-                    </q-item>
-                    <q-separator />
-                    <q-item v-ripple clickable @click="handleLockAsync">
-                        <q-item-section avatar>
-                            <q-icon :name="mdiLock" color="warning" />
-                        </q-item-section>
-                        <q-item-section class="text-warning"> Lock </q-item-section>
-                    </q-item>
-                    <q-separator />
-                    <q-item v-ripple clickable @click="handleRemoveVaultDataAsync">
-                        <q-item-section avatar>
-                            <q-icon :name="mdiTrashCanOutline" color="negative" />
-                        </q-item-section>
-                        <q-item-section class="text-negative text-no-wrap"> Remove Vault Data </q-item-section>
-                    </q-item>
+                    <template v-for="(item, index) in menuItems" :key="index">
+                        <q-item v-if="!item.separator" v-ripple clickable :to="item.to" @click="item.onClick">
+                            <q-item-section avatar>
+                                <q-icon :name="item.icon" :color="item.color" />
+                            </q-item-section>
+                            <q-item-section
+                                :class="[{ 'text-no-wrap': item.noWrap }, item.color && `text-${item.color}`]"
+                            >
+                                {{ item.label }}
+                            </q-item-section>
+                        </q-item>
+                        <q-separator v-else />
+                    </template>
                 </q-menu>
             </q-btn>
         </q-toolbar>
@@ -70,4 +51,37 @@ const { handleRemoveVaultDataAsync } = useRemoveVaultData();
 const handleLockAsync = async () => {
     await tryCatchWrapperAsync(() => vaultStore.lockAsync());
 };
+
+const menuItems = [
+    {
+        to: "/wallets",
+        icon: mdiWallet,
+        label: "Wallets",
+    },
+    {
+        to: "/history",
+        icon: mdiHistory,
+        label: "History",
+    },
+    {
+        to: "/password",
+        icon: mdiShieldLock,
+        label: "Password",
+    },
+    { separator: true },
+    {
+        onClick: handleLockAsync,
+        icon: mdiLock,
+        label: "Lock",
+        color: "warning",
+    },
+    { separator: true },
+    {
+        onClick: handleRemoveVaultDataAsync,
+        icon: mdiTrashCanOutline,
+        label: "Remove Vault Data",
+        color: "negative",
+        noWrap: true,
+    },
+];
 </script>
