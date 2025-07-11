d57fc1b11d4a3edaa24b8763d3592a326683c47f
stotiks <47013208+stotiks@users.noreply.github.com>
2025-07-12 00:03:47 +0300

    refactor: adjust layout styles in UnlockPage for improved alignment

--- ui/src-bex/entrypoints/popup/pages/UnlockPage/index.vue
+++ ui/src-bex/entrypoints/popup/pages/UnlockPage/index.vue
@@ -1,21 +1,24 @@
 <template>
-    <q-page padding style="padding-top: 66px">
-        <div class="fullscreen row justify-center">
-            <q-card flat class="self-center col-xl-4 col-lg-6 col-md-8 col-sm-10 col-xs-10">
-                <q-card-section>
-                    <UnlockForm />
-                </q-card-section>
-                <q-card-section>
-                    <q-btn
-                        no-caps
-                        label="Remove Vault Data"
-                        color="negative"
-                        size="sm"
-                        flat
-                        dense
-                        @click="handleRemoveVaultDataAsync"
-                /></q-card-section>
-            </q-card>
+    <q-page padding>
+        <div class="fixed-center full-width">
+            <div class="row justify-center">
+                <q-card flat class="col-xl-4 col-lg-6 col-md-8 col-sm-10 col-xs-10">
+                    <q-card-section>
+                        <UnlockForm />
+                    </q-card-section>
+                    <q-card-section>
+                        <q-btn
+                            no-caps
+                            label="Remove Vault Data"
+                            color="negative"
+                            size="sm"
+                            flat
+                            dense
+                            @click="handleRemoveVaultDataAsync"
+                        />
+                    </q-card-section>
+                </q-card>
+            </div>
         </div>
     </q-page>
 </template>
