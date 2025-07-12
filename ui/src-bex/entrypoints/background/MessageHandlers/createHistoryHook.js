6bf479f8d67a4665bec6bc74fba5f239adc5ebed
stotiks <47013208+stotiks@users.noreply.github.com>
2025-07-12 03:20:30 +0300

    bigint fix

--- ui/src-bex/entrypoints/background/MessageHandlers/createHistoryHook.js
+++ ui/src-bex/entrypoints/background/MessageHandlers/createHistoryHook.js
@@ -1,19 +1,5 @@
-import { JSONbigNativeString } from "@/mmx/wallet/utils/JSONbigNative";
 import vault from "../stores/vault";
 
-class Context {
-    #stringify(...options) {
-        return JSONbigNativeString.stringify(this, ...options);
-    }
-
-    toString() {
-        return this.#stringify();
-    }
-
-    toJSON() {
-        return JSONbigNativeString.parse(JSONbigNativeString.stringify({ ...this }));
-    }
-}
 export const createHistoryHook = () => {
     return async (context) => {
         const { handler, result } = context;
@@ -21,7 +7,7 @@ export const createHistoryHook = () => {
         if (isAcceptRequired) {
             const { data, ...restResult } = result;
 
-            const ctx = Object.assign(new Context(), { ...context, result: restResult });
+            const ctx = { ...context, result: restResult };
             vault.addHistoryAsync(ctx);
         }
     };
