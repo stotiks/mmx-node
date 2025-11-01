12b8c08a5bac19f826b872ff27457f8bd8f18558
stotiks <47013208+stotiks@users.noreply.github.com>
2025-11-02 00:41:33 +0200

    WriteBytes test

--- ui/src/mmx/wallet/common/WriteBytes.js
+++ ui/src/mmx/wallet/common/WriteBytes.js
@@ -148,8 +148,8 @@ export class WriteBytes extends WriteBuffer {
                 this.write_bytes_map(value);
             } else if (value.constructor?.name && isBytes(value)) {
                 this.write_bytes_bytes(value);
-            } else if (value instanceof bytes_t) {
-                this.write_bytes_bytes(value.valueOf());
+                // } else if (value instanceof bytes_t) {
+                //     this.write_bytes_bytes(value.valueOf());
             } else if (value instanceof txin_t) {
                 this.write_bytes_txin_t(value, full_hash);
             } else if (value instanceof txout_t) {
