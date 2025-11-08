373c2c0218f34e8e32b1ea46acc245c882bb213b
stotiks <47013208+stotiks@users.noreply.github.com>
2025-11-08 18:21:06 +0200

    Enhance bytes_t and hash_t classes with improved input validation and string representation

--- ui/src/mmx/wallet/common/addr_t.js.test.js
+++ ui/src/mmx/wallet/common/addr_t.js.test.js
@@ -1,8 +1,33 @@
 import { describe, it, assert, expect } from "vitest";
 import { hexToBytes } from "@noble/hashes/utils.js";
-import { addr_t } from "./addr_t";
+import { addr_t, bytes_t, hash_t } from "./addr_t";
 
 import "../utils/Uint8ArrayUtils";
+
+describe("bytes_t", () => {
+    it("empty to bytes_t", () => {
+        const bytes = new bytes_t();
+        assert.equal(bytes.toString(), "");
+    });
+
+    const int8Array = Uint8Array.from([0xca, 0xfe, 0x01, 0x23]);
+    const str = int8Array.toHex();
+
+    it("Uint8Array to bytes_t", () => {
+        const int8Array = Uint8Array.from([0xca, 0xfe, 0x01, 0x23]);
+        const bytes = new bytes_t(int8Array);
+        assert.equal(bytes.toString(), str);
+    });
+
+    it("str to bytes_t", () => {
+        const bytes = new bytes_t(str);
+        assert.equal(bytes.toString(), str);
+    });
+
+    it("invalid input type", () => {
+        expect(() => new bytes_t(666)).toThrowError();
+    });
+});
 
 describe("addr_t", () => {
     const addrStr = "mmx1ckyz0x7fpet4y7zmckyg7lklp8dc5gdr2kjd8hamk49rnk8zu9eq2cnz7a";
@@ -34,12 +59,35 @@ describe("addr_t", () => {
         expect(() => new addr_t(invalidAddr)).toThrowError();
     });
 
-    it("invalid address type", () => {
+    it("invalid input type", () => {
         expect(() => new addr_t(0)).toThrowError();
     });
 
     it("invalid length", () => {
         expect(() => new addr_t(new Uint8Array(0))).toThrowError();
         expect(() => new addr_t(new Uint8Array(31))).toThrowError();
+    });
+});
+
+describe("hash_t", () => {
+    it("empty to bytes_t", () => {
+        const hash = new hash_t();
+        assert.equal(hash.toString(), "0000000000000000000000000000000000000000000000000000000000000000");
+    });
+
+    it("str to hash", () => {
+        const str = "test";
+        const hash = new hash_t(str);
+        assert.equal(hash.toString(), "9F86D081884C7D659A2FEAA0C55AD015A3BF4F1B2B0B822CD15D6C15B0F00A08");
+    });
+
+    it("bytes to hash", () => {
+        const bytes = hexToBytes("FF");
+        const hash = new hash_t(bytes);
+        assert.equal(hash.toString(), "A8100AE6AA1940D0B663BB31CD466142EBBDBD5187131B92D93818987832EB89");
+    });
+
+    it("invalid input type", () => {
+        expect(() => new hash_t(666)).toThrowError();
     });
 });
