571fa9394b04fec3f47304edd161699fb78aec1c
stotiks <47013208+stotiks@users.noreply.github.com>
2025-12-07 00:52:40 +0200

    src/mmx/wallet/mnemonic.js

--- ui/src/mmx/wallet/mnemonic.js.test.js
+++ ui/src/mmx/wallet/mnemonic.js.test.js
@@ -1,22 +1,28 @@
-import { assert, describe, expect, it } from "vitest";
-import { mnemonicToSeed, seedToWords, randomWords } from "./mnemonic";
+import { assert, describe, expect, it, vi } from "vitest";
+import { mnemonicToSeed, seedToWords, randomWords, randomSeed } from "./mnemonic";
 
 import "./utils/Uint8ArrayUtils";
 import { hash_t } from "./common/addr_t";
-import { hexToBytes } from "@noble/hashes/utils.js";
+import { wordlist as wordlistEnglish } from "@scure/bip39/wordlists/english.js";
+
+import * as nobleUtils from "@noble/hashes/utils.js";
+vi.mock("@noble/hashes/utils.js", { spy: true });
+
+const mnemonic =
+    "apple apple apple apple apple apple apple apple apple apple apple apple apple apple apple apple apple apple apple apple apple apple apple anchor";
+const hashStr = "A84215AA50852A54A10A55A84215AA50852A54A10A55A84215AA50852A54A10A";
+const seed = nobleUtils.hexToBytes(hashStr);
 
 describe("mnemonic #1", async () => {
-    const mnemonic =
-        "apple apple apple apple apple apple apple apple apple apple apple apple apple apple apple apple apple apple apple apple apple apple apple anchor";
-    const hashStr = "0AA1542A8550AA1542A8550AA1542A8550AA1542A8550AA1542A8550AA1542A8";
-
     it("mnemonicToSeed", () => {
-        const seed = mnemonicToSeed(mnemonic);
-        assert.equal(seed.toReversed().toHex(), hashStr);
+        const seed2 = mnemonicToSeed(mnemonic);
+        assert.deepEqual(seed2, seed);
+
+        const seed3 = mnemonicToSeed(mnemonic, wordlistEnglish);
+        assert.deepEqual(seed3, seed);
     });
 
     it("seedToWords", () => {
-        const seed = hexToBytes(hashStr);
         const words = seedToWords(seed);
         assert.equal(words, mnemonic);
     });
@@ -25,23 +31,32 @@ describe("mnemonic #1", async () => {
 describe("mnemonic #2", async () => {
     const mnemonic =
         "abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon art";
-    const hashStr = new hash_t().toString();
+    const seed = new hash_t();
 
     it("mnemonicToSeed", () => {
-        const seed = mnemonicToSeed(mnemonic);
-        assert.equal(seed.toReversed().toHex(), hashStr);
+        const seed2 = mnemonicToSeed(mnemonic);
+        assert.deepEqual(seed2, seed);
     });
 
     it("seedToWords", () => {
-        const seed = hexToBytes(hashStr);
-        const words = seedToWords(seed);
-        assert.equal(words, mnemonic);
+        const mnemonic2 = seedToWords(seed);
+        assert.equal(mnemonic2, mnemonic);
     });
 });
 
 describe("mnemonic #3", async () => {
+    vi.mocked(nobleUtils.randomBytes).mockReturnValue(seed);
+
+    it("randomSeed", () => {
+        const seed2 = randomSeed();
+        assert.deepEqual(seed2, seed);
+    });
+
     it("randomWords", () => {
-        const mnemonic = randomWords();
-        assert.isNotEmpty(mnemonic);
+        const mnemonic2 = randomWords();
+        assert.equal(mnemonic2, mnemonic);
+
+        const mnemonic3 = randomWords(wordlistEnglish);
+        assert.equal(mnemonic3, mnemonic);
     });
 });
