ccca834577fd46a328cdb727d2add51ee91cf452
stotiks <47013208+stotiks@users.noreply.github.com>
2025-12-09 21:11:32 +0200

    fix

--- ui/src/mmx/wallet/common/ECDSAUtils/promisify/utils/execFunction.js
+++ ui/src/mmx/wallet/common/ECDSAUtils/promisify/utils/execFunction.js
@@ -1,23 +1,7 @@
-const callWithNamedParams = (fn, params) => {
-    const getParamNames = (fn) => {
-        const fnStr = fn.toString().replace(/\s+/g, " ");
-        const params = fnStr.match(/\((.*?)\)/)[1];
-        return params ? params.split(",").map((p) => p.trim()) : [];
-    };
-
-    // Extract function parameter names
-    const paramNames = getParamNames(fn);
-
-    // Create arguments array in correct order
-    const args = paramNames.map((name) => params[name]);
-
-    return fn(...args);
-};
-
 const executeFunction = (fnName, args, functionList) => {
     const fn = functionList[fnName];
     if (fn) {
-        return callWithNamedParams(fn, args);
+        return fn.apply(null, Object.values(args));
     } else {
         throw new Error(`Unknown fnName: ${fnName}`);
     }
