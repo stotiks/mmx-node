34c678dd57e57b2752c18264ee762dca7e673298
stotiks <47013208+stotiks@users.noreply.github.com>
2025-12-09 16:06:18 +0200

    execECDSAFunction

--- ui/src/mmx/wallet/common/ECDSAUtils/promisify/worker.js
+++ ui/src/mmx/wallet/common/ECDSAUtils/promisify/worker.js
@@ -1,9 +1,8 @@
-import { syncFunctionList as functionList } from "@/mmx/wallet/common/ECDSAUtils/ECDSAUtils";
-import { executeFunctionWithCallbacks } from "./utils/executeFunction";
+import { executeECDSAFunctionWithCallbacks } from "./utils/execECDSAFunction";
 
 self.onmessage = function (e) {
     const { fnName, args } = e.data;
     const resolve = (result) => self.postMessage({ success: true, result });
     const reject = (error) => self.postMessage({ success: false, error });
-    executeFunctionWithCallbacks(fnName, args, functionList, resolve, reject);
+    executeECDSAFunctionWithCallbacks(fnName, args, resolve, reject);
 };
