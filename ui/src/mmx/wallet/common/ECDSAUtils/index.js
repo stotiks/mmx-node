8ad8876cbc1ca5e322cdf5b14783dbb19abde3b7
stotiks <47013208+stotiks@users.noreply.github.com>
2025-12-09 21:11:32 +0200

    fix

--- ui/src/mmx/wallet/common/ECDSAUtils/index.js
+++ ui/src/mmx/wallet/common/ECDSAUtils/index.js
@@ -2,14 +2,14 @@ import { promisify } from "./promisify";
 import { getFarmerKey, getKeys, getAddress, getFingerPrint, sign, signAsync } from "./ECDSAUtils";
 
 const getFingerPrintAsync = async (seed_value, passphrase) =>
-    await promisify("getFingerPrint", { seed_value, passphrase });
+    await promisify("getFingerPrint", [seed_value, passphrase]);
 
-const getFarmerKeyAsync = async (seed_value) => await promisify("getFarmerKey", { seed_value });
+const getFarmerKeyAsync = async (seed_value) => await promisify("getFarmerKey", [seed_value]);
 
 const getAddressAsync = async (seed_value, passphrase, index) =>
-    await promisify("getAddress", { seed_value, passphrase, index });
+    await promisify("getAddress", [seed_value, passphrase, index]);
 
-const getKeysAsync = async (seed_value, passphrase, index) => promisify("getKeys", { seed_value, passphrase, index });
+const getKeysAsync = async (seed_value, passphrase, index) => promisify("getKeys", [seed_value, passphrase, index]);
 
 export {
     getFarmerKey,
