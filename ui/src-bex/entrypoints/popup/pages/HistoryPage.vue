9aa9176457420766f9b38a88dbba04858218ffd3
stotiks <47013208+stotiks@users.noreply.github.com>
2025-12-16 22:00:09 +0200

    refactor

--- ui/src-bex/entrypoints/popup/pages/HistoryPage.vue
+++ ui/src-bex/entrypoints/popup/pages/HistoryPage.vue
@@ -5,8 +5,9 @@
             <q-list bordered separator>
                 <q-item v-for="(item, index) in history" :key="index">
                     <q-item-section>
-                        <q-item-label>{{ item.method }}</q-item-label>
-                        <q-item-label caption>{{ new Date(item.time).toLocaleString() }}</q-item-label>
+                        <q-item-label>{{ item.message?.data?.method }}</q-item-label>
+                        <q-item-label caption>{{ item.activeWallet }}</q-item-label>
+                        <q-item-label caption>{{ new Date(item.timestamp).toLocaleString() }}</q-item-label>
                     </q-item-section>
                     <q-item-section v-if="item.result" side>
                         <q-icon
