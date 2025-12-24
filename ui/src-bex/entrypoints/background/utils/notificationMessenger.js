981d58ff62ff0f9c2b9b127eed51f8e332a1e727
stotiks <47013208+stotiks@users.noreply.github.com>
2025-12-24 22:03:59 +0200

    rename

--- ui/src-bex/entrypoints/background/utils/notificationMessenger.js
+++ ui/src-bex/entrypoints/background/utils/notificationMessenger.js
@@ -1,11 +1,11 @@
-import { openNotification } from "@bex/entrypoints/background/utils/openNotification";
+import { openNotificationAsync } from "@bex/entrypoints/background/utils/openNotification";
 import { backgroundMessenger } from "@bex/messaging/entrypointMessengers/background";
 
 const _sendMessageAsync = async (payload) =>
     await backgroundMessenger.sendMessageAsync("notification", payload, "popup");
 
 const sendMessageAsync = async (payload) => {
-    await openNotification();
+    await openNotificationAsync();
     return await _sendMessageAsync(payload);
 };
 
