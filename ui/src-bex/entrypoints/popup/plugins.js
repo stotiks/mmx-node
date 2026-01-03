53784d310102245f3fb55a26b547bbe7a0b17bc4
stotiks <47013208+stotiks@users.noreply.github.com>
2026-01-03 07:18:10 +0200

    cleanup

--- ui/src-bex/entrypoints/popup/plugins.js
+++ ui/src-bex/entrypoints/popup/plugins.js
@@ -1,6 +1,5 @@
 // Plugins
 import { Quasar, quasarConfig } from "@/plugins/quasar";
-// import pinia from "@/plugins/pinia";
 import router from "./router";
 import i18n from "@/plugins/i18n";
 import VueQueryPlugin, { vueQueryPluginOptions } from "@/plugins/query";
@@ -8,12 +7,6 @@ import highlight from "@/plugins/highlight";
 
 import { createPinia } from "pinia";
 const pinia = createPinia();
-pinia.use(({ store }) => {
-    // store.router = markRaw(router);
-    // store.queryClient = markRaw(useQueryClient());
-});
-
-// export default pinia;
 
 export function registerPlugins(app) {
     app.use(Quasar, quasarConfig);
