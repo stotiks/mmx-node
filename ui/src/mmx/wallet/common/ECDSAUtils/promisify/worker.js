9a8bdd34c2447dd1f55bfad38a896d22b83305c6
stotiks <47013208+stotiks@users.noreply.github.com>
2026-01-17 23:45:24 +0200

    refactor

--- ui/src/mmx/wallet/common/ECDSAUtils/promisify/worker.js
+++ ui/src/mmx/wallet/common/ECDSAUtils/promisify/worker.js
@@ -1,8 +1,8 @@
-import { executeECDSAFunctionWithCallbacks } from "./utils/execECDSAFunction";
+import { executeECDSAFunctionAsync } from "./utils/execECDSAFunctionAsync";
 
 self.onmessage = async (event) => {
     const { fnName, args } = event.data;
     const resolve = (result) => self.postMessage({ success: true, result });
     const reject = (error) => self.postMessage({ success: false, error });
-    await executeECDSAFunctionWithCallbacks(fnName, args, resolve, reject);
+    await executeECDSAFunctionAsync(fnName, args).then(resolve).catch(reject);
 };
