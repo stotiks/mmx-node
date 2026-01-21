749a764f1e654b9fd7a916b4f1822ba2be1350cf
stotiks <47013208+stotiks@users.noreply.github.com>
2026-01-21 21:34:02 +0200

    feat: add pre-hooks to block ECDSA wallet access and enforce vault lock state

--- ui/src-bex/entrypoints/background/MessageHandlers/VaultMessageHandler/index.js
+++ ui/src-bex/entrypoints/background/MessageHandlers/VaultMessageHandler/index.js
@@ -1,4 +1,8 @@
 import { MessageHandler } from "@bex/messaging/MessageHandler";
 import vault from "@bex/entrypoints/background/Vault";
+import { createBlockECDSAWalletHook } from "./hooks/createBlockECDSAWalletHook";
+import { createVaultLockHook } from "./hooks/createVaultLockHook";
 
 export const vaultMessageHandler = new MessageHandler(vault);
+vaultMessageHandler.addPreHook(createVaultLockHook());
+vaultMessageHandler.addPreHook(createBlockECDSAWalletHook());
