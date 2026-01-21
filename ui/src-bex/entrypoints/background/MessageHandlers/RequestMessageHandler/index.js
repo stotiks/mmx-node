ae4b7d82cdd598aedeadfc66c9978699d62fc603
stotiks <47013208+stotiks@users.noreply.github.com>
2026-01-21 23:18:46 +0200

    reorg

--- ui/src-bex/entrypoints/background/MessageHandlers/RequestMessageHandler/index.js
+++ ui/src-bex/entrypoints/background/MessageHandlers/RequestMessageHandler/index.js
@@ -3,7 +3,6 @@ import { requestMessageHandlerMethods } from "./requestMessageHandlerMethods";
 import { createAuthHook } from "./hooks/createAuthHook";
 import { createHistoryHook } from "./hooks/createHistoryHook";
 
-const requestMessageHandler = new MessageHandler(requestMessageHandlerMethods);
+export const requestMessageHandler = new MessageHandler(requestMessageHandlerMethods);
 requestMessageHandler.addPreHook(createAuthHook());
 requestMessageHandler.addPostHook(createHistoryHook());
-export { requestMessageHandler };
