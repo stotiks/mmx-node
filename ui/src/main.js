b8e8b6d9b2123d2d6c2633c37ac99cd52438a53d
stotiks <47013208+stotiks@users.noreply.github.com>
2026-01-22 03:44:24 +0200

    refactor: remove createQuasarApp function and directly create app instance in main.js

--- ui/src/main.js
+++ ui/src/main.js
@@ -1,6 +1,13 @@
+import "quasar/src/css/index.sass";
+import "animate.css";
+import "unfonts.css";
+import "@/css/app.scss";
+
+import { createApp } from "vue";
+
 import App from "./App.vue";
 import { registerPlugins } from "./plugins";
 
-import { createQuasarApp } from "@/createQuasarApp";
-const app = createQuasarApp(App, registerPlugins);
+const app = createApp(App);
+registerPlugins(app);
 app.mount("#app");
