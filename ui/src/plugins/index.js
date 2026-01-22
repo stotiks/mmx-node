9f8ee0dcc7321c39c529c1cf24f6960c8413dfeb
stotiks <47013208+stotiks@users.noreply.github.com>
2026-01-22 03:44:24 +0200

    refactor: remove createQuasarApp function and directly create app instance in main.js

--- ui/src/plugins/index.js
+++ ui/src/plugins/index.js
@@ -6,11 +6,11 @@ import pinia from "@/plugins/pinia";
 import VueQueryPlugin, { vueQueryPluginOptions } from "@/plugins/query";
 import highlight from "@/plugins/highlight";
 
-export function registerPlugins(app) {
+export const registerPlugins = (app) => {
     app.use(Quasar, quasarConfig);
     app.use(router);
     app.use(i18n);
     app.use(pinia);
     app.use(VueQueryPlugin, vueQueryPluginOptions);
     app.use(highlight);
-}
+};
