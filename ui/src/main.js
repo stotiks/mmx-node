6dc45a0bc6131c3e787a1916715e7af41bfc08f6
stotiks <47013208+stotiks@users.noreply.github.com>
2026-01-24 15:21:21 +0200

    revert

--- ui/src/main.js
+++ ui/src/main.js
@@ -1,13 +1,6 @@
-import "quasar/src/css/index.sass";
-import "animate.css";
-import "unfonts.css";
-import "@/css/app.scss";
-
-import { createApp } from "vue";
-
 import App from "./App.vue";
 import { registerPlugins } from "./plugins";
 
-const app = createApp(App);
-registerPlugins(app);
+import { createQuasarApp } from "@/createQuasarApp";
+const app = createQuasarApp(App, registerPlugins);
 app.mount("#app");
