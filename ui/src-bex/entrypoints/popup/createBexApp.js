3d43c8435bba7e2ccfac05425dcc27fee96e0127
stotiks <47013208+stotiks@users.noreply.github.com>
2026-01-25 12:52:03 +0200

    refactor: restructure plugin registration and remove unused plugins

--- ui/src-bex/entrypoints/popup/createBexApp.js
+++ ui/src-bex/entrypoints/popup/createBexApp.js
@@ -1,10 +1,18 @@
 import App from "@bex/entrypoints/popup/App.vue";
-import { registerPlugins } from "@bex/entrypoints/popup/plugins";
-
 import { createQuasarApp } from "@/createQuasarApp";
+import { registerPlugins } from "@/plugins/registerPlugins";
+import router from "@bex/entrypoints/popup/router";
+import { createPinia } from "pinia";
 
+/**
+ * Creates and mounts a BEX app instance
+ * @param {boolean} isNotification - Whether this is a notification window
+ */
 export const createBexApp = (isNotification = false) => {
-    const app = createQuasarApp(App, registerPlugins);
+    const pinia = createPinia();
+    const app = createQuasarApp(App);
+
+    registerPlugins(app, { router, pinia, includeHighlight: false });
     app.provide("isNotification", isNotification);
     app.mount("#app");
 };
