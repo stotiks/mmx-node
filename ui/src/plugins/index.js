a976cc3f462871ff50befb0b86ce2427ff54d35d
stotiks <47013208+stotiks@users.noreply.github.com>
2026-01-25 12:52:03 +0200

    refactor: restructure plugin registration and remove unused plugins

--- ui/src/plugins/index.js
+++ ui/src/plugins/index.js
@@ -1,16 +1,2 @@
-// Plugins
-import { Quasar, quasarConfig } from "@/plugins/quasar";
-import router from "@/plugins/router";
-import i18n from "@/plugins/i18n";
-import pinia from "@/plugins/pinia";
-import VueQueryPlugin, { vueQueryPluginOptions } from "@/plugins/query";
-import highlight from "@/plugins/highlight";
-
-export const registerPlugins = (app) => {
-    app.use(Quasar, quasarConfig);
-    app.use(router);
-    app.use(i18n);
-    app.use(pinia);
-    app.use(VueQueryPlugin, vueQueryPluginOptions);
-    app.use(highlight);
-};
+// Re-export for backward compatibility
+export { registerPlugins } from "@/plugins/registerPlugins";
