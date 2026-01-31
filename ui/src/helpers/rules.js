e3c7bc992031486f7a5514026b95bebad683aea0
stotiks <47013208+stotiks@users.noreply.github.com>
2026-02-01 01:03:08 +0200

    i18n

--- ui/src/helpers/rules.js
+++ ui/src/helpers/rules.js
@@ -1,21 +1,25 @@
+import i18n from "@/plugins/i18n";
+
+const { t } = i18n.global;
+
 const rules = {
     // is empty or non-negative number
-    number: (value) => !value || /^\d+$/.test(value) || "Invalid number", //TODO i18n
+    number: (value) => !value || /^\d+$/.test(value) || t("validation.invalid_number"),
 
     // is empty or valid mmx address
-    address: (value) => !value || validateAddress(value) || "Invalid address", //TODO i18n
+    address: (value) => !value || validateAddress(value) || t("validation.invalid_address"),
 
     // is non empty
-    required: (value) => !isEmpty(value) || "Field is required", //TODO i18n
+    required: (value) => !isEmpty(value) || t("validation.field_required"),
 
     amount: (value) => {
         //if (value && value.length && value.match(/^(\d+([.,]\d*)?)$/)) {
-        return isEmpty(value) || (typeof value === "number" && value > 0) || "Invalid amount"; //TODO i18n
+        return isEmpty(value) || (typeof value === "number" && value > 0) || t("validation.invalid_amount");
     },
 
     memo: (value) => {
         if (value && value.length > 64) {
-            return "Maximum length is 64"; //TODO i18n
+            return t("validation.max_length", { max: 64 });
         }
         return true;
     },
