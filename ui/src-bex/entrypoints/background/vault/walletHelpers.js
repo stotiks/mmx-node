2d1b174b336f2092f30b4aae93c35eace82ef482
stotiks <47013208+stotiks@users.noreply.github.com>
2026-02-04 01:10:42 +0200

    feat: implement secure wallet instance destruction and cleanup

--- ui/src-bex/entrypoints/background/vault/walletHelpers.js
+++ ui/src-bex/entrypoints/background/vault/walletHelpers.js
@@ -3,6 +3,24 @@ import { Wallet } from "@/mmx/wallet/Wallet";
 import vault from "@bex/entrypoints/background/vault";
 import { bytesToHex } from "@noble/hashes/utils.js";
 
+/**
+ * Execute a callback function with an ECDSA wallet instance and ensure cleanup.
+ * This function gets the ECDSA wallet, calls the callback, and destroys the wallet
+ * to securely clean up sensitive cryptographic data from memory.
+ *
+ * @param {string} address - The wallet address
+ * @param {Function} callback - Async function that receives the ecdsaWallet instance
+ * @returns {Promise<*>} The result of the callback function
+ */
+const withECDSAWallet = async (address, callback) => {
+    const ecdsaWallet = await vault.getECDSAWalletAsync({ address });
+    try {
+        return await callback(ecdsaWallet);
+    } finally {
+        ecdsaWallet.destroy();
+    }
+};
+
 const getWalletByAddress = async (address) => {
     if (!address) {
         throw new Error("No wallet selected");
@@ -16,15 +34,16 @@ export const getCurrentWallet = () => {
 };
 
 export const getPubKeyAsync = async (address = vault.getCurrentWalletAddress()) => {
-    const ecdsaWallet = await vault.getECDSAWalletAsync({ address });
-    const { pubKey } = await ecdsaWallet.getKeysAsync(0);
-
-    return bytesToHex(pubKey).toUpperCase();
+    return withECDSAWallet(address, async (ecdsaWallet) => {
+        const { pubKey } = await ecdsaWallet.getKeysAsync(0);
+        return bytesToHex(pubKey).toUpperCase();
+    });
 };
 
 export const signMessageAsync = async (msg, address = vault.getCurrentWalletAddress()) => {
-    const ecdsaWallet = await vault.getECDSAWalletAsync({ address });
-    return await ecdsaWallet.signMsgAsync(address, msg);
+    return withECDSAWallet(address, async (ecdsaWallet) => {
+        return await ecdsaWallet.signMsgAsync(address, msg);
+    });
 };
 
 // const getMojoAmountAsync = async (amount, currency, options) => {
@@ -50,16 +69,18 @@ export const getSendTxAsync = async (
     options,
     address = vault.getCurrentWalletAddress()
 ) => {
-    const ecdsaWallet = await vault.getECDSAWalletAsync({ address });
-    if (!currency) {
-        currency = new addr_t().toString();
-    }
-    // const mojoAmount = await getMojoAmountAsync(amount, currency, options);
-    const tx = await Wallet.getSendTxAsync(ecdsaWallet, amount, dst_addr, currency, options);
-    return tx;
+    return withECDSAWallet(address, async (ecdsaWallet) => {
+        if (!currency) {
+            currency = new addr_t().toString();
+        }
+        // const mojoAmount = await getMojoAmountAsync(amount, currency, options);
+        const tx = await Wallet.getSendTxAsync(ecdsaWallet, amount, dst_addr, currency, options);
+        return tx;
+    });
 };
 
 export const signTransactionAsync = async (tx, options, address = vault.getCurrentWalletAddress()) => {
-    const ecdsaWallet = await vault.getECDSAWalletAsync({ address });
-    await ecdsaWallet.signOfAsync(tx, options);
+    return withECDSAWallet(address, async (ecdsaWallet) => {
+        await ecdsaWallet.signOfAsync(tx, options);
+    });
 };
