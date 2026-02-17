e10ce6096d99f456a4a5dfee86bb54f92062707a
stotiks <47013208+stotiks@users.noreply.github.com>
2026-02-18 00:52:38 +0200

    vaultPublicAPI

--- ui/src-bex/entrypoints/background/MessageHandlers/vaultMessageHandler/index.js
+++ ui/src-bex/entrypoints/background/MessageHandlers/vaultMessageHandler/index.js
@@ -1,8 +1,44 @@
 import vault from "@bex/entrypoints/background/vault";
 import { MessageHandler } from "@bex/messaging/MessageHandler";
-import createBlockECDSAWalletHook from "./hooks/createBlockECDSAWalletHook";
 
-const vaultMessageHandler = new MessageHandler(vault);
-vaultMessageHandler.addPreHook(createBlockECDSAWalletHook());
+/**
+ * Whitelist of safe vault methods that can be accessed via message handler.
+ * This explicitly excludes:
+ * - getECDSAWalletAsync: exposes raw private key material
+ * - on, removeListener: event methods used server-side only
+ */
+const vaultPublicAPI = {
+    // Storage manager interface
+    getIsInitializedAsync: vault.getIsInitializedAsync,
+    initAsync: vault.initAsync,
+    getIsUnlocked: vault.getIsUnlocked,
+    unlockAsync: vault.unlockAsync,
+    lock: vault.lock,
+    clearAllAsync: vault.clearAllAsync,
+    updatePasswordAsync: vault.updatePasswordAsync,
+
+    // Wallet interface
+    getNetworkAsync: vault.getNetworkAsync,
+    getWalletsAsync: vault.getWalletsAsync,
+    addWalletAsync: vault.addWalletAsync,
+    removeWalletAsync: vault.removeWalletAsync,
+    getCurrentWalletAddress: vault.getCurrentWalletAddress,
+    setCurrentWalletByAddressAsync: vault.setCurrentWalletByAddressAsync,
+    // getECDSAWalletAsync: EXCLUDED - exposes raw private key material
+
+    // History interface
+    getHistoryAsync: vault.getHistoryAsync,
+    addHistoryAsync: vault.addHistoryAsync,
+    clearHistoryAsync: vault.clearHistoryAsync,
+    getHistoryCountAsync: vault.getHistoryCountAsync,
+
+    // Permission interface
+    checkUrlPermissionsAsync: vault.checkUrlPermissionsAsync,
+    allowUrlAsync: vault.allowUrlAsync,
+    revokeUrlAsync: vault.revokeUrlAsync,
+    getAllowedOrigins: vault.getAllowedOrigins,
+};
+
+const vaultMessageHandler = new MessageHandler(vaultPublicAPI);
 
 export default vaultMessageHandler;
