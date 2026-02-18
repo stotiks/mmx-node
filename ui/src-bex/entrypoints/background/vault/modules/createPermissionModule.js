6c86632e80ab63395b747fadd0c85218674c7863
stotiks <47013208+stotiks@users.noreply.github.com>
2026-02-18 02:30:45 +0200

    comments

--- ui/src-bex/entrypoints/background/vault/modules/createPermissionModule.js
+++ ui/src-bex/entrypoints/background/vault/modules/createPermissionModule.js
@@ -6,6 +6,7 @@ export const createPermissionModule = (dependencies = {}) => {
     }
 
     // Private permission storage - Set of allowed origins
+    // Per-session storage only, no persistence needed - permissions reset on browser restart
     const allowedOriginsSet = new Set();
 
     /**
