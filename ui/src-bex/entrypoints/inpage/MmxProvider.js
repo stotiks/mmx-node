a8bde8449da3833ded934f747f6712550d63b763
stotiks <47013208+stotiks@users.noreply.github.com>
2026-02-22 21:00:22 +0200

    private event methods

--- ui/src-bex/entrypoints/inpage/MmxProvider.js
+++ ui/src-bex/entrypoints/inpage/MmxProvider.js
@@ -39,29 +39,31 @@ export class MmxProvider {
 
     request = async (payload) => await this.#sendMessageAsync("provider/request", payload);
 
+    // Private events map - not accessible from outside
+    #events = new Map();
+
     constructor() {
         windowMessenger.onMessage("message", (message) => {
             const { eventName, data } = message.data;
-            this.emit(eventName, data);
+            this.#emit(eventName, data);
             return { success: true };
         });
 
         return createReadonlyProxy(this);
     }
 
-    // events
-    _events = new Map();
+    // Public API for event listeners
     on = (eventName, callback) => {
-        if (!this._events.has(eventName)) {
-            this._events.set(eventName, []);
+        if (!this.#events.has(eventName)) {
+            this.#events.set(eventName, []);
         }
-        this._events.get(eventName).push(callback);
+        this.#events.get(eventName).push(callback);
         return this;
     };
 
     removeListener(eventName, callback) {
-        if (this._events.has(eventName)) {
-            const callbacks = this._events.get(eventName);
+        if (this.#events.has(eventName)) {
+            const callbacks = this.#events.get(eventName);
             const index = callbacks.indexOf(callback);
             if (index > -1) {
                 callbacks.splice(index, 1);
@@ -70,9 +72,10 @@ export class MmxProvider {
         return this;
     }
 
-    emit(eventName, ...args) {
-        if (this._events.has(eventName)) {
-            this._events.get(eventName).forEach((callback) => {
+    // Private emit method - only callable internally
+    #emit(eventName, ...args) {
+        if (this.#events.has(eventName)) {
+            this.#events.get(eventName).forEach((callback) => {
                 try {
                     callback(...args);
                 } catch (err) {
