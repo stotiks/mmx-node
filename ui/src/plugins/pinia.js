a42284f23fcdb9a1e2a481955012bbf127d10567
stotiks <47013208+stotiks@users.noreply.github.com>
2026-03-01 00:13:19 +0200

    cleanup

--- ui/src/plugins/pinia.js
+++ ui/src/plugins/pinia.js
@@ -1,13 +1,5 @@
-// Utilities
-import router from "@/plugins/router";
 import { createPinia } from "pinia";
-// import { vueQueryPluginOptions } from "@/plugins/query";
 
 const pinia = createPinia();
-
-// pinia.use(({ store }) => {
-//     store.router = markRaw(router);
-//     store.queryClient = markRaw(vueQueryPluginOptions.queryClient);
-// });
 
 export default pinia;
