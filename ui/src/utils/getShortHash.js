a997f9b1816938ab2b32af888258daa68f22b541
stotiks <47013208+stotiks@users.noreply.github.com>
2026-03-01 23:30:13 +0200

    truncateMiddle

--- ui/src/utils/getShortHash.js
+++ ui/src/utils/getShortHash.js
@@ -1,6 +1,11 @@
+import { truncateMiddle } from "./truncateMiddle.js";
+
+/**
+ * @deprecated Use truncateMiddle instead
+ */
 export const getShortHash = (hash, length) => {
     if (!length) {
         length = 10;
     }
-    return hash.substring(0, length) + "..." + hash.substring(64 - length);
+    return truncateMiddle(hash, length, length);
 };
