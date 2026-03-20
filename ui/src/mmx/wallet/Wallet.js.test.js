f23c45bfb8c38af0de4fb2f6eab57b46af7f497b
stotiks <47013208+stotiks@users.noreply.github.com>
2026-03-21 00:02:07 +0200

    misc

--- ui/src/mmx/wallet/Wallet.js.test.js
+++ ui/src/mmx/wallet/Wallet.js.test.js
@@ -231,10 +231,13 @@ describe("Wallet", () => {
         // const bid_currency_decimals = 6;
         // const bid_currency_symbol = "🐳";
 
-        const amount = 100001;
+        const amount = 100001; //value = "0.100001"
 
         const _price = 1000;
-        const price = get_inv_price(_price); //"0x4189374bc6a7f0"
+        const price = get_inv_price(_price); //"0x4189374bc6a7f0" = 18446744073709552
+
+        //{"input": {"amount": "100000", "value": "0.1"}, "inv_price": "18446744073709552", "next_input": {"amount": "100001", "value": "0.100001"}, "trade": {"amount": "100", "value": "0.0001"}}
+        //{"input": {"amount": "100001", "value": "0.100001"}, "inv_price": "18446744073709552", "next_input": {"amount": "101000", "value": "0.101"}, "trade": {"amount": "100", "value": "0.0001"}}
 
         const tx = await Wallet.getOfferTradeTxAsync(ecdsaWallet, address, amount, ask_currency, price, options);
 
