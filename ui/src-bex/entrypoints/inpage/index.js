1f877b36de1b2452cfd9bc702c98bdfad4c4ccb1
stotiks <47013208+stotiks@users.noreply.github.com>
2026-03-21 02:37:34 +0200

    isBexLoaded fix

--- ui/src-bex/entrypoints/inpage/index.js
+++ ui/src-bex/entrypoints/inpage/index.js
@@ -2,10 +2,18 @@ import { defineUnlistedScript } from "#imports";
 import { MmxProvider } from "./MmxProvider";
 
 export default defineUnlistedScript(async () => {
+    const provider = new MmxProvider();
     Object.defineProperty(window, "mmx", {
-        value: new MmxProvider(),
+        value: provider,
         writable: false,
         configurable: false,
     });
+    document.dispatchEvent(
+        new CustomEvent("mmx-provider-loaded", {
+            detail: {
+                provider,
+            },
+        })
+    );
     console.info("🧩 Fury Vault: provider loaded");
 });
