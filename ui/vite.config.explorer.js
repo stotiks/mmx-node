773039d1f9a7a628095df9f021a6199826781948
stotiks <47013208+stotiks@users.noreply.github.com>
2026-03-22 22:48:00 +0200

    config refactor

--- ui/vite.config.explorer.js
+++ ui/vite.config.explorer.js
@@ -2,10 +2,6 @@ import { ConfigBuilder, BuildTargets } from "./vite.ConfigBuilder";
 
 const configBuilder = new ConfigBuilder({
     buildTarget: BuildTargets.EXPLORER,
-    writeBuildInfo: true,
-    writeRobotsTxt: true,
-    usePublicRPC: true,
-    usePublicRPCForDevMode: true,
 });
 
 export default configBuilder.config;
