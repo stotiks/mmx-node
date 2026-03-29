b76927bd05c45f86844ba64ee8175c72fdbb7868
stotiks <47013208+stotiks@users.noreply.github.com>
2026-03-29 23:59:01 +0300

    todo bex router auth

--- ui/src-bex/entrypoints/popup/router/routes.js
+++ ui/src-bex/entrypoints/popup/router/routes.js
@@ -3,20 +3,24 @@ export default [
         name: "home",
         path: "/",
         component: () => import("@bex/entrypoints/popup/pages/MainPage"),
+        // meta: { requiresAuth: true },
     },
 
     {
         path: "/password",
         component: () => import("@bex/entrypoints/popup/pages/Settings/PasswordPage"),
+        meta: { requiresAuth: true },
     },
 
     {
         path: "/wallets",
         component: () => import("@bex/entrypoints/popup/pages/Settings/WalletsPage"),
+        meta: { requiresAuth: true },
     },
 
     {
         path: "/history",
         component: () => import("@bex/entrypoints/popup/pages/HistoryPage"),
+        meta: { requiresAuth: true },
     },
 ];
