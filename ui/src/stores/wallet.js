2db187d8fdc862a2c8160d257136e4287b8ca8e4
stotiks <47013208+stotiks@users.noreply.github.com>
2026-03-31 22:58:38 +0300

    onUnmounted

--- ui/src/stores/wallet.js
+++ ui/src/stores/wallet.js
@@ -22,6 +22,10 @@ export const useWalletStore = defineStore("wallet", () => {
         clearWallet();
     };
 
+    onUnmounted(() => {
+        clearWallet();
+    });
+
     return {
         wallet,
         setWallet,
