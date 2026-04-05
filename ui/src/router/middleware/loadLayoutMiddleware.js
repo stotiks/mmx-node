7b7c6bc53e54c2791a059f98b2a504cedb847f34
stotiks <47013208+stotiks@users.noreply.github.com>
2026-04-06 00:19:47 +0300

    misc

--- ui/src/router/middleware/loadLayoutMiddleware.js
+++ ui/src/router/middleware/loadLayoutMiddleware.js
@@ -1,10 +1,10 @@
 export const loadLayoutMiddleware = async (to) => {
     if (to.meta.layout === false) {
-        to.meta.layoutComponent = "div";
+        to.meta.layoutComponent = null;
         return;
     }
 
     const layout = to.meta.layout ?? "default";
-    const layoutComponent = await import(`@/layouts/${layout}.vue`);
-    to.meta.layoutComponent = layoutComponent.default;
+    const { default: layoutComponent } = await import(`@/layouts/${layout}.vue`);
+    to.meta.layoutComponent = layoutComponent;
 };
