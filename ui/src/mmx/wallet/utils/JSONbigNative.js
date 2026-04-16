010347155a411c08d57ae7ad7a3d8987d013d754
stotiks <47013208+stotiks@users.noreply.github.com>
2026-04-17 00:41:37 +0300

    rm json-bigint

--- ui/src/mmx/wallet/utils/JSONbigNative.js
+++ ui/src/mmx/wallet/utils/JSONbigNative.js
@@ -1,3 +1,34 @@
-import JSONbig from "json-bigint";
-export const JSONbigNative = JSONbig({ alwaysParseAsBig: false, useNativeBigInt: true });
-export const JSONbigNativeString = JSONbig({ alwaysParseAsBig: false, useNativeBigInt: true, storeAsString: true });
+const JSONbig = ({ storeAsString = false } = {}) => {
+    const bigIntToRawJSON = (key, val) => {
+        return typeof val === "bigint" ? JSON.rawJSON(String(val)) : val;
+    };
+
+    const rawJSONToBigInt = (key, val, { source }) => {
+        if (typeof source === "string") {
+            try {
+                const bigInt = BigInt(source);
+                if (bigInt > Number.MAX_SAFE_INTEGER || bigInt < Number.MIN_SAFE_INTEGER) {
+                    if (storeAsString) {
+                        return String(bigInt);
+                    } else {
+                        return bigInt;
+                    }
+                } else {
+                    return Number.parseInt(bigInt);
+                }
+            } catch (e) {
+                return val;
+            }
+        } else {
+            return val;
+        }
+    };
+
+    return {
+        parse: (text) => JSON.parse(text, rawJSONToBigInt),
+        stringify: (obj, replacer, ...args) => JSON.stringify(obj, bigIntToRawJSON, ...args),
+    };
+};
+
+export const JSONbigNative = JSONbig();
+export const JSONbigNativeString = JSONbig({ storeAsString: true });
